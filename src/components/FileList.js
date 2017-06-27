134811602d04f97a82c53b84f7237298f77a0107
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-06-27 14:22:46 +0900

    add action icon

--- src/components/FileList.js
+++ src/components/FileList.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { Table, TableHeader, TableHeaderColumn, TableBody, TableRow, TableRowColumn } from "material-ui/Table";
+import IconButton from 'material-ui/IconButton';
+import ActionInfo from 'material-ui/svg-icons/action/info';
+import ActionDelete from 'material-ui/svg-icons/action/delete';
+import ImageEdit from "material-ui/svg-icons/image/edit";
 
 const FileList = ({
   files
@@ -11,6 +15,7 @@ const FileList = ({
           <TableRow>
             <TableHeaderColumn>id</TableHeaderColumn>
             <TableHeaderColumn>name</TableHeaderColumn>
+            <TableHeaderColumn>action</TableHeaderColumn>
           </TableRow>
         </TableHeader>
 
@@ -20,6 +25,17 @@ const FileList = ({
               <TableRow key={file.id}>
                 <TableRowColumn>{file.id}</TableRowColumn>
                 <TableRowColumn>{file.name}</TableRowColumn>
+                <TableRowColumn>
+                  <IconButton>
+                    <ActionInfo />
+                  </IconButton>
+                  <IconButton>
+                    <ImageEdit />
+                  </IconButton>
+                  <IconButton>
+                    <ActionDelete />
+                  </IconButton>
+                </TableRowColumn>
               </TableRow>
             );
           })}
