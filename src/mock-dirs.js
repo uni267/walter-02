f3e1ce1bafc41b69fa5596c41453414134106d65
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-06-28 19:17:18 +0900

    export mock

---
+++ src/mock-dirs.js
@@ -0,0 +1,6 @@
+const DIRS = [
+  {id: 1, name: "dir01"},
+  {id: 2, name: "dir02"},
+];
+
+export default DIRS;
---
+++ src/mock-files.js
@@ -0,0 +1,7 @@
+const FILES = [
+  {id: 1, name: "file01.txt", is_dir: false},
+  {id: 2, name: "file02.txt", is_dir: false},
+  {id: 3, name: "dir01", is_dir: true},
+];
+
+export default FILES;
