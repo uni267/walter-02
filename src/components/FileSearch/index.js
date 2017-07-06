8c2fc1ae246b9192c805c999cbb0e36703a9a848
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-07-06 13:33:31 +0900

    file search(1)

--- src/components/FileSearch/index.js
+++ src/components/FileSearch/index.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import TextField from "material-ui/TextField";
 
-const FileSearch = () => {
-  return (
-    <div className="file-search">
-      <TextField
-        hintText=""
-        floatingLabelText="search"
-      />
-    </div>
-  );
+class FileSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: ""
+    };
+  }
+
+  render() {
+    const handleChange = (e) => {
+      this.setState({ searchText: e.target.value });
+      console.log(this.state.searchText);
+    };
+
+    return (
+      <div className="file-search">
+        <TextField
+          value={this.state.searchText}
+          onChange={handleChange}
+          hintText=""
+          floatingLabelText="search"
+          />
+      </div>
+    );
+  }
 };
 
 export default FileSearch;
