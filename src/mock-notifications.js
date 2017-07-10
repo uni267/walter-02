485ac1884fbce94507cca890fe984b7618f88ab9
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-07-10 14:54:12 +0900

    fix mock

--- src/mock-notifications.js
+++ src/mock-notifications.js
@@ -1,19 +1,19 @@
 const NOTIFICATIONS = [
   {
     id: 1,
-    title: "notification-03",
+    title: "おしらせ03",
     body: "おしらせの内容が入ります",
     modified: "2017-07-01 10:00"
   },
   {
     id: 2,
-    title: "notification-02",
+    title: "おしらせ02",
     body: "おしらせの内容が入ります",
     modified: "2017-06-01 10:00"
   },
   {
     id: 3,
-    title: "notification-01",
+    title: "おしらせ01",
     body: "おしらせの内容が入ります",
     modified: "2017-05-01 10:00"
   }
