964635bbb67b01deccc2ae6648281bf7927d9903
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-07-10 14:54:36 +0900

    directoryリストのセパレート

--- src/components/DirBox/index.js
+++ src/components/DirBox/index.js
@@ -13,7 +13,7 @@ const styles = {
   dir_list: {
     marginTop: 10,
     marginLeft: 10,
-    padding: 10
+    padding: 5
   },
   dir: {
     color: "#555",
@@ -24,22 +24,34 @@ const styles = {
 
 class DirBox extends Component {
   renderDir(dir) {
-    return (
-      <div key={dir.id} style={styles.dir_list}>
-        <Link to={`/?dir_id=${dir.id}`} style={styles.dir}>
-          {dir.name}
-        </Link>
-      </div>
-    );
+    if (dir === "sep") {
+      return (
+        <div style={styles.dir_list}>&gt;</div>
+      );
+    } else {
+      return (
+        <div key={dir.id} style={styles.dir_list}>
+          <Link to={`/?dir_id=${dir.id}`} style={styles.dir}>
+            {dir.name}
+          </Link>
+        </div>
+      );
+    }
   }
 
   render() {
+    let dirs = [].concat.apply([], this.props.dirs.map( (dir, idx) => {
+      return (idx === 0) ? dir : ["sep", dir];
+    }));
+
+
     return (
       <div className="dir-box" style={styles.wrapper}>
-        {this.props.dirs.map((dir, idx) => this.renderDir(dir, idx))}
+        {dirs.map((dir, idx) => this.renderDir(dir, idx))}
       </div>
     );
   }
 }
 
 export default DirBox;
+// [].concat.apply([], [...Array(5).keys()].map( (v, i) => [v, ">"]))
