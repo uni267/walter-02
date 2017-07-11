8a67afa444082a14a5c05f8aad9e8b6066a20471
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-07-11 14:22:40 +0900

    componentをディレクトリに分割したので削除

--- src/components/__tests__/FileBox.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { shallow, mount } from "enzyme";
-
-describe("FileBox.js", () => {
-  it("empty", () => {
-    expect(true).toEqual(true);
-  });
-});
