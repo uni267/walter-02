997b300b07a07687c388b7fb825c15c7ee72853b
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-07-12 18:15:05 +0900

    権限、変更履歴を追加

--- src/mock-files.js
+++ src/mock-files.js
@@ -10,13 +10,72 @@ const FILES = [
   },
   {
     id: 1,
-    name: "xxxx社契約書2017年1月.doc",
+    name: "xxx様の仕様書2017_01.doc",
     modified: "2017-01-20 9:30",
     owner: "user01",
     is_dir: false,
     dir_id: 0,
     is_display: true,
-    is_star: false
+    is_star: false,
+    histories: [
+      {
+        id: 3,
+        user: { name: "user01" },
+        action: "ファイル名を変更",
+        modified: "2016-12-01 10:00",
+        body: "旧ファイル名はxxy.doc, 新ファイル名はxyy.doc"
+      },
+      {
+        id: 2,
+        user: { name: "user01" },
+        action: "ファイル名を変更",
+        modified: "2016-06-01 10:00",
+        body: "旧ファイル名はxxx.doc, 新ファイル名はxxy.doc"
+      },
+      {
+        id: 1,
+        user: { name: "user01" },
+        action: "ファイルを新規作成",
+        modified: "2016-06-01 10:00",
+        body: "新ファイル名はxxx.doc"
+      }
+    ],
+    authorities: [
+      {
+        group: {
+          id: 1,
+          name: "営業部",
+          members: [
+            {id: 1, name: "user01"},
+            {id: 2, name: "user02"}
+          ]
+        },
+        role: {
+          read: true,
+          modify: true,
+          delete: false,
+          restore: false,
+          authory: false
+        }
+      },
+      {
+        group: {
+          id: 2,
+          name: "情報システム部",
+          members: [
+            {id: 3, name: "admin01"},
+            {id: 4, name: "admin02"}
+          ]
+        },
+        role: {
+          read: true,
+          modify: true,
+          delete: true,
+          restore: true,
+          authory: true
+        }
+      }
+    ]
   },
   {
     id: 2,
@@ -26,7 +85,59 @@ const FILES = [
     is_dir: false,
     dir_id: 0,
     is_display: true,
-    is_star: false
+    is_star: false,
+    histories: [
+      {
+        id: 2,
+        user: { name: "user01" },
+        action: "ファイル名変更",
+        modified: "2016-12-01 10:00",
+        body: "旧: xxy.doc, 新: xyy.doc"
+      },
+      {
+        id: 1,
+        user: { name: "user01" },
+        action: "ファイル名変更",
+        modified: "2016-06-01 10:00",
+        body: "旧: xxx.doc, 新: xxy.doc"
+      }
+    ],
+    authorities: [
+      {
+        group: {
+          id: 1,
+          name: "営業部",
+          members: [
+            {id: 1, name: "user01"},
+            {id: 2, name: "user02"}
+          ]
+        },
+        role: {
+          read: true,
+          modify: true,
+          delete: false,
+          restore: false,
+          authory: false
+        }
+      },
+      {
+        group: {
+          id: 2,
+          name: "情報システム部",
+          members: [
+            {id: 3, name: "admin01"},
+            {id: 4, name: "admin02"}
+          ]
+        },
+        role: {
+          read: true,
+          modify: true,
+          delete: true,
+          restore: true,
+          authory: true
+        }
+      }
+    ]
   },
   {
     id: 3,
@@ -36,7 +147,45 @@ const FILES = [
     is_dir: false,
     dir_id: 0,
     is_display: true,
-    is_star: true
+    is_star: true,
+    histories: [
+      {
+        id: 2,
+        user: { name: "user01" },
+        action: "ファイル名変更",
+        modified: "2016-12-01 10:00",
+        body: "旧: xxy.doc, 新: xyy.doc"
+      },
+      {
+        id: 1,
+        user: { name: "user01" },
+        action: "ファイル名変更",
+        modified: "2016-06-01 10:00",
+        body: "旧: xxx.doc, 新: xxy.doc"
+      }
+    ],
+    authorities: [
+      {
+        group: { id: 1, name: "営業部" },
+        role: {
+          read: true,
+          modify: true,
+          delete: false,
+          restore: false,
+          authory: false
+        }
+      },
+      {
+        group: { id: 1, name: "情報システム部" },
+        role: {
+          read: true,
+          modify: true,
+          delete: true,
+          restore: true,
+          authory: true
+        }
+      }
+    ]
   },
   {
     id: 4,
