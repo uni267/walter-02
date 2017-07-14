c1121908df1e565f7d40a64cf36f162e474db28b
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-07-14 16:59:01 +0900

    名称変更

---
+++ src/mock-users.js
@@ -0,0 +1,23 @@
+const USERS = [
+  {
+    id: 1,
+    type: "user",
+    name: "user01",
+    name_jp: "ユーザ 太郎"
+  },
+  {
+    id: 2,
+    type: "user",
+    name: "user02",
+    name_jp: "ユーザ 次郎"
+  },
+  {
+    id: 3,
+    type: "group",
+    name: "grp_soumu",
+    name_jp: "総務グループ"
+  }
+];
+
+export default USERS;
+  
