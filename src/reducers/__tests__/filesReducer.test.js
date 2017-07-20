a72503087baab9b28a3123fb68afe670450cc96c
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-07-20 18:22:44 +0900

    ci test

--- src/reducers/__tests__/filesReducer.test.js
+++ src/reducers/__tests__/filesReducer.test.js
@@ -4,6 +4,10 @@ import FILES from "../../mock-files";
 describe("filesReducer", () => {
 
   describe("default", () => {
+    it("CI test", () => {
+      expect(false).toEqual(true);
+    });
+
     it("ファイル一覧が返却される", () => {
       const result = filesReducer(FILES, {});
 
