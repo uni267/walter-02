a26a011d17a3078ec300a808c107b79dbe684802
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-07-26 13:25:21 +0900

    タグマスタ

---
+++ src/mock-tags.js
@@ -0,0 +1,19 @@
+const TAGS = [
+  {
+    id: 1,
+    color: "#c62828",
+    label: "重要"
+  },
+  {
+    id: 2,
+    color: "#42A5F5",
+    label: "業務"
+  },
+  {
+    id: 3,
+    color: "#bdbdbd",
+    label: "その他"
+  }
+];
+
+export default TAGS;
