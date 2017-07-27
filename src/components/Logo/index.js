4bced322f600f3474e005985bf3470ea6b06e815
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-07-27 11:22:41 +0900

    不要なファイルを削除

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -11,6 +11,10 @@ import Avatar from "material-ui/Avatar";
 import Badge from 'material-ui/Badge';
 import NotificationsIcon from 'material-ui/svg-icons/social/notifications';
 import { Card, CardHeader, CardText } from 'material-ui/Card';
+import Dialog from "material-ui/Dialog";
+import FlatButton from 'material-ui/FlatButton';
+import TextField from "material-ui/TextField";
+import Drawer from 'material-ui/Drawer';
 
 // router
 import { Link } from "react-router-dom";
@@ -26,6 +30,107 @@ const styles = {
 };
 
 class Logo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu: {
+        open: false
+      },
+      account: {
+        open: false
+      }
+    };
+  }
+
+  toggleAppMenu = () => {
+    this.setState({
+      menu: {
+        open: !this.state.menu.open
+      }
+    });
+  };
+
+  renderAccount = () => {
+
+    const onAccountClose = () => {
+      this.setState({
+        account: { open: false }
+      });
+    };
+
+    const actions = [
+      (
+        <FlatButton
+          label="close"
+          onTouchTap={onAccountClose}
+          />
+      ),
+      (
+        <FlatButton
+          label="save"
+          primary={true}
+          onTouchTap={onAccountClose}
+          />
+      )
+    ];
+
+    return (
+      <Dialog
+        title="アカウント情報変更"
+        modal={true}
+        actions={actions}
+        open={this.state.account.open}
+        onRequestClose={onAccountClose}
+        >
+        <TextField
+          hintText=""
+          floatingLabelText="current password"
+          type="password"
+          />
+
+        <br />
+        
+        <TextField
+          hintText=""
+          floatingLabelText="new password"
+          type="password"
+          />
+
+      </Dialog>
+    );
+  };
+
+  renderMenu = () => {
+    const menus = [
+      {name: "ファイル一覧"},
+      {name: "管理コンソール"},
+    ];
+
+    const renderMenu = (menu, idx) => {
+      return (
+        <MenuItem
+          key={idx}
+          onTouchTap={this.toggleAppMenu}
+          primaryText={menu.name}
+          />
+      );
+    };
+
+    return (
+      <Drawer
+        docked={false}
+        open={this.state.menu.open}
+        width={200}
+        onRequestChange={this.toggleAppMenu}
+        >
+
+        {menus.map( (menu, idx) => renderMenu(menu, idx) )}
+
+      </Drawer>
+      
+    );
+  };
+
   renderRightElements = () => {
 
     const notificationIcon = (
@@ -80,13 +185,14 @@ class Logo extends Component {
           <Divider />
           <MenuItem
             primaryText="アカウント情報変更"
-            onTouchTap={this.props.onAccountClick} />
+            onTouchTap={() => this.setState({ account: { open: true } })} />
           <Divider />
           <MenuItem primaryText="ログアウト" />
         </IconMenu>
       </div>
     );
   }
+
   render() {
     const title = (
       <Link to={`/home/`} style={styles.title}>
@@ -98,9 +204,12 @@ class Logo extends Component {
       <div className="logo">
         <AppBar
           title={title}
-          iconElementRight={this.renderRightElements}
-          onLeftIconButtonTouchTap={this.props.onMenuIconClick}
-          />
+          iconElementRight={this.renderRightElements()}
+          onLeftIconButtonTouchTap={this.toggleAppMenu} />
+
+        {this.renderMenu()}
+        {this.renderAccount()}
+
       </div>
     );
   }
--- src/containers/NavigationContainer.js
+++ src/containers/NavigationContainer.js
@@ -5,27 +5,12 @@ import { connect } from "react-redux";
 
 // components
 import Logo from "../components/Logo/";
-import AppMenu from "../components/AppMenu/";
-import Account from "../components/Account/";
-
-// actions
-import { toggleAccount, toggleMenu } from "../actions";
 
 class NavigationContainer extends Component {
   render() {
     return (
       <div>
-        <Logo
-          onAccountClick={this.props.onAccountClick}
-          onMenuIconClick={this.props.onMenuIconClick}
-          notifications={this.props.notifications} />
-
-        <AppMenu open={this.props.appMenu.open}
-                 toggleDrawer={this.props.onMenuIconClick} />
-
-        <Account 
-          account={this.props.account}
-          onAccountClick={this.props.onAccountClick} />
+        <Logo notifications={this.props.notifications} />
       </div>
     );
   }
@@ -33,15 +18,11 @@ class NavigationContainer extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    appMenu: state.appMenu,
-    account: state.account,
     notifications: state.notifications
   };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  onAccountClick: () => { dispatch(toggleAccount()); },
-  onMenuIconClick: () => { dispatch(toggleMenu()); }
 });
 
 NavigationContainer = connect(
