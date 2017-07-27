f6e125bf613bd9e5044e447e99850265b76bd0dd
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-07-27 11:45:30 +0900

    container -> component 移譲

--- src/containers/FileBoxContainer.js
+++ src/containers/FileBoxContainer.js
@@ -32,39 +32,6 @@ const styles = {
 
 class FileBoxContainer extends Component {
 
-  dirRoute = (dir) => {
-    let result = [];
-
-    const walk = (dir) => {
-      if (Number(dir.id) === 0) {
-        result = [{ id: 0 }];
-        return;
-      }
-
-      const dirs = this.props.dirs.slice();
-      const target = dirs
-            .filter(d => d.descendant === Number(dir.id) && d.depth === 1)[0];
-
-      // top node
-      if (target.ancestor === 0) {
-        result = [{ id: target.descendant }, ...result];
-        result = [{ id: target.ancestor }, ...result];
-        return;
-      } else {
-        result = [{ id: target.descendant }, ...result];
-        walk({ id: target.ancestor });
-      }
-
-    };
-
-    walk(dir);
-    return result;
-  };
-
-  addDirName = (dir) => {
-    return this.props.allFiles.filter(file => dir.id == file.id)[0];
-  };
-
   render() {
     let _files = this.props.files.slice();
 
@@ -77,14 +44,14 @@ class FileBoxContainer extends Component {
       }).filter(file => file.is_display);
     }
 
-    const dirs = this.dirRoute({ id: this.props.dir_id })
-        .map(this.addDirName);
-
     return (
       <Card>
         <div style={{display: "flex"}}>
           <div style={{width: "40%"}}>
-            <DirBox dirs={dirs} />
+            <DirBox
+              allFiles={this.props.allFiles}
+              dirId={this.props.dir_id}
+              dirs={this.props.dirs} />
           </div>
 
           <div style={{width: "60%"}}>
