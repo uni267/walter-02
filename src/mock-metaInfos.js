296e4ad5f19e4ed37cefce1ad333838d538f8693
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-07-31 15:05:03 +0900

    メタ情報

---
+++ src/mock-metaInfos.js
@@ -0,0 +1,7 @@
+const META_INFOS = [
+  {id: 1, text: "契約書No", value: "契約書No"},
+  {id: 2, text: "顧客名", value: "顧客名"},
+  {id: 3, text: "契約金額", value: "契約金額"},
+];
+
+export default META_INFOS;
--- src/reducers/filesReducer.js
+++ src/reducers/filesReducer.js
@@ -195,10 +195,27 @@ const filesReducer = (state = FILES, action) => {
     file = {...file, id: nextFileId};
     return [...state, file];
 
+  case "ADD_META_INFO":
+    return state.map(file => {
+      if (file.id === action.file.id) {
+        file.metaInfo = [...file.metaInfo, action.metaInfo];
+      }
+      return file;
+    });
+
+  case "DELETE_META_INFO":
+    return state.map(file => {
+      if (file.id === action.file.id) {
+        file.metaInfo = file.metaInfo.filter(
+          meta => meta.id !== action.metaInfo.id
+        );
+      }
+      return file;
+    });
+
   default:
     return state;
   }
-
 };
 
 export default filesReducer;
