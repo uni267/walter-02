413a4f12966d950b63f20986b058a19a1f70a6ee
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-08-18 11:39:34 +0900

    ファイル一覧、フォルダルートを並列化

--- src/sagas/watchFetchFiles.js
+++ src/sagas/watchFetchFiles.js
@@ -1,26 +1,34 @@
 import { delay } from "redux-saga";
-import { call, put, fork, take } from "redux-saga/effects";
+import { call, put, fork, take, all, select } from "redux-saga/effects";
 
 // api
-import { login, fetchUserById, fetchFiles } from "../apis";
+import { fetchFiles, fetchDirs } from "../apis";
 
 function* watchFetchFiles() {
   while (true) {
-    const task = yield take("REQUEST_FETCH_FILES");
+    const { dir_id } = yield take("REQUEST_FETCH_FILES");
     yield put({ type: "LOADING_START" });
 
     try {
-      const files = yield call(fetchFiles, task.dir_id);
-      console.log(files.data.body);
+      yield call(delay, 500);
+
+      const [ files, dirs ] = yield all([
+        call(fetchFiles, dir_id),
+        call(fetchDirs, dir_id)
+      ]);
+
       yield put({ type: "INIT_FILE", files: files.data.body });
-      yield put({ type: "LOADING_END" });
+      yield put({ type: "INIT_DIR", dirs: dirs.data.body });
+      const _dirs = yield select(state => state.dirs);
+      console.log(_dirs);
     }
     catch (e) {
       console.log(e);
-      const message = e.response.data.status.message;
-      const errors = e.response.data.status.errors;
+    }
+    finally {
       yield put({ type: "LOADING_END" });
     }
+
   }
 }
 
