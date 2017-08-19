8f248a22f865e4ff1a5f78d07895719183d9996a
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-08-19 17:32:23 +0900

    不要なコンテナを削除

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -3,21 +3,79 @@ import React, { Component } from "react";
 // store
 import { connect } from "react-redux";
 
+// material ui
+import { Card } from 'material-ui/Card';
+
 // containers
 import NavigationContainer from "./NavigationContainer";
-import FileBoxContainer from "./FileBoxContainer";
+import FileActionContainer from "./FileActionContainer";
+import FileSearch from "../components/FileSearch";
+import DirBox from "../components/DirBox";
+import FileListContainer from "./FileListContainer";
+import FileSnackbar from "../components/FileSnackbar";
+
+// actions
+import {
+  triggerSnackbar,
+  closeSnackbar,
+  searchFile,
+  requestFetchFiles
+}
+from "../actions";
 
 class HomeContainer extends Component {
+  componentWillMount() {
+    this.props.requestFetchFiles(this.props.match.params.id);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.match.params.id !== nextProps.match.params.id) {
+      this.props.requestFetchFiles(nextProps.match.params.id);
+    }
+  }
+
   render() {
-    const params = new URLSearchParams(this.props.location.search);
-    const dirId = params.get("dir_id") === null
-          ? this.props.tenant.dirId
-          : params.get("dir_id");
-          
+    const dirId = this.props.match.params.id;
+
     return (
       <div>
         <NavigationContainer />
-        <FileBoxContainer dir_id={dirId} />
+
+        <Card>
+          <div style={{display: "flex"}}>
+            <div style={{width: "40%"}}>
+              <DirBox
+                dirId={dirId}
+                dirs={this.props.dirs} />
+            </div>
+
+            <div style={{width: "60%"}}>
+              <FileSearch
+                searchWord={this.props.searchWord}
+                searchFile={this.props.searchFile} />
+            </div>
+          </div>
+
+          <div style={{display: "flex"}}>
+            <div style={{width: "78%"}}>
+
+              <FileListContainer
+                dir_id={dirId}
+                files={this.props.files} />
+
+            </div>
+
+            <div style={{width: "22%"}}>
+              <FileActionContainer dir_id={dirId} />
+            </div>
+
+            <FileSnackbar
+              closeSnackbar={this.props.closeSnackbar}
+              snackbar={this.props.snackbar} />
+
+          </div>
+        </Card>
+
       </div>
     );
   }
@@ -25,10 +83,24 @@ class HomeContainer extends Component {
 
 const mapStateToProps = (state, ownProps) => {
   return {
+    snackbar: state.snackbar,
+    searchWord: state.searchFile,
+    fileSortTarget: state.fileSortTarget,
+    files: state.files,
+    dirs: state.dirs,
+    session: state.session,
+    loading: state.loading,
     tenant: state.tenant
   };
 };
-  
-HomeContainer = connect(mapStateToProps)(HomeContainer);
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  triggerSnackbar: (message) => { dispatch(triggerSnackbar(message)); },
+  closeSnackbar: () => { dispatch(closeSnackbar()); },
+  searchFile: (value) => { dispatch(searchFile(value)); },
+  requestFetchFiles: (dir_id) => { dispatch(requestFetchFiles(dir_id)); }
+});
+
+HomeContainer = connect(mapStateToProps, mapDispatchToProps)(HomeContainer);
 
 export default HomeContainer;
