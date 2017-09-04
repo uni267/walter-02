d37ec5a26e4c2adb7c6c20f78d164bb9f1b7bf3a
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-09-04 13:52:15 +0900

    リファクタ

--- server/lib/routes/login.js
+++ server/lib/routes/login.js
@@ -3,53 +3,70 @@ import crypto from "crypto";
 import jwt from "jsonwebtoken";
 import { SECURITY_CONF } from "../../configs/server";
 import User from "../models/User";
+import Tenant from "../models/Tenant";
 
 const router = Router();
 
 router.post("/", (req, res, next) => {
 
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (user === null) {
-      res.status(401);
-      res.json({
-        status: {
-          success: false,
-          message: "認証に失敗しました",
-          errors: { name: "ユーザが存在しません" }
-        },
-        body: {}
-      });
-      return;
-    }
+  User.findOne({ email: req.body.email })
+    .then( user => {
+      if (user === null) throw "user not found";
+      return user;
+    })
+    .then( user => {
 
-    const sha = crypto.createHash("sha512");
-    sha.update(req.body.password);
-    const hash = sha.digest("hex");
+      const sha = crypto.createHash("sha512");
+      sha.update(req.body.password);
+      const hash = sha.digest("hex");
 
-    if (user.password === hash) {
+      if (user.password !== hash) throw "password is invalid";
 
       const { secretKey } = SECURITY_CONF.development;
       const token = jwt.sign(user, secretKey, { expiresIn: "24h" });
 
+      res.token = token;
+      return user;
+    })
+    .then( user => {
+      res.user = user.toObject();
+
+      return Tenant.findOne(user.tenant_id);
+    })
+    .then( tenant => {
+      res.user.tenant = tenant;
+
       res.json({
         status: { success: true, message: "ログインに成功しました" },
-        token: token,
-        body: { user_id: user._id }
+        body: {
+          token: res.token,
+          user: res.user
+        }
       });
+    })
+    .catch( err => {
+      let errors;
 
-    } else {
-      res.status(401).json({
+      switch ( err ) {
+      case "user not found":
+        errors = { name: "ユーザが存在しません" };
+        break;
+      case "password is invalid":
+        errors = { password: "パスワードに誤りがあります" };
+        break;
+      default:
+        errors = err;
+        break;
+      }
+
+      res.status(400).json({
         status: {
           success: false,
           message: "認証に失敗しました",
-          errors: { password: "パスワードに誤りがあります" }
-        },
-        body: {}
+          errors: errors
+        }
       });
-    }
-
-  });
-
+    });
 });
 
 export default router;
