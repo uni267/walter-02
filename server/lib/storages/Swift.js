00b223135c1826d3f59ab92c3f41fb6bcf75e8f9
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-09-10 14:43:27 +0900

    export defaultだと何故か例外なのでexport classする

--- server/lib/storages/Swift.js
+++ server/lib/storages/Swift.js
@@ -1,24 +1,37 @@
 import pkgcloud from "pkgcloud";
 import { STORAGE_CONF } from "../../configs/server";
 
-export const getClient = () => {
-  const mode = process.env.NODE_ENV;
-  let config;
+class Swift {
+  constructor(params) {
 
-  switch (mode) {
-  default:
-    config = STORAGE_CONF.development;
-    break;
+    const { tenant_name } = params;
+
+    if (tenant_name === undefined || 
+        tenant_name === null ||
+        tenant_name === "") throw "params.tenant_name not defined";
+
+    this.tenant_name = tenant_name;
+
+    const mode = process.env.NODE_ENV;
+    let config;
+
+    switch (mode) {
+    default:
+      config = STORAGE_CONF.development;
+      break;
+    }
+    
+    this.client = pkgcloud.storage.createClient(config);
   }
-  
-  return pkgcloud.storage.createClient(config);
-};
 
-export const getContainers = (client) => {
-  return new Promise( (resolve, reject) => {
-    client.getContainers( (err, containers) => {
-      if (err) return reject(err);
-      return resolve(containers);
+  getContainers() {
+    return new Promise( (resolve, reject) => {
+      this.client.getContainers( (err, containers) => {
+        if (err) return reject(err);
+        return resolve(containers);
+      });
     });
-  });
-};
+  }
+}
+
+export { Swift };
