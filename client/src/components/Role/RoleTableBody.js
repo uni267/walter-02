fe7f3276ef386af1d2fb7896a65a5da8b4693a92
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-09-11 09:52:07 +0900

    refactor

---
+++ client/src/components/Role/RoleTableBody.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// router
+import { Link } from "react-router-dom";
+
+// material ui
+import { TableRow, TableRowColumn } from 'material-ui/Table';
+import IconButton from 'material-ui/IconButton';
+import ImageEdit from "material-ui/svg-icons/image/edit";
+import Chip from "material-ui/Chip";
+
+const RoleTableBody = ({
+  role,
+  key
+}) => {
+  return (
+    <TableRow key={key}>
+      <TableRowColumn>{role.name}</TableRowColumn>
+      <TableRowColumn>{role.description}</TableRowColumn>
+      <TableRowColumn>
+        <div style={{ display: "flex" }}>
+          {role.actions.map( (action, idx) => {
+            return <Chip key={idx} style={{ marginRight: 10 }}>{action.label}</Chip>;
+          })}
+        </div>
+      </TableRowColumn>
+      <TableRowColumn>
+        <IconButton containerElement={<Link to={`/roles/${role._id}`} />}>
+          <ImageEdit />
+        </IconButton>
+      </TableRowColumn>
+    </TableRow>
+  );
+};
+
+RoleTableBody.propTypes = {
+  role: PropTypes.object.isRequired
+};
+
+export default RoleTableBody;
---
+++ client/src/components/Role/RoleTableHeader.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// material ui
+import { TableHeaderColumn, TableRow } from 'material-ui/Table';
+
+const RoleTableHeader = ({
+  headers
+}) => {
+  return (
+    <TableRow>
+      {headers.map( (header, idx) => (
+        <TableHeaderColumn key={idx}>{header.name}</TableHeaderColumn>
+      ))}
+    </TableRow>
+  );
+};
+
+RoleTableHeader.propTypes = {
+  headers: PropTypes.array.isRequired
+};
+
+export default RoleTableHeader;
