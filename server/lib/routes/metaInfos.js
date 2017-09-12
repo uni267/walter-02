ba6e54384a2c7795da0ff6af73c63dd54170d47c
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-09-12 19:12:58 +0900

    promise -> generator

--- server/lib/routes/metaInfos.js
+++ server/lib/routes/metaInfos.js
@@ -1,25 +1,44 @@
 import mongoose from "mongoose";
-
+import co from "co";
 import { Router } from "express";
 import MetaInfo from "../models/MetaInfo";
 
 const router = Router();
 
 router.get("/", (req, res, next) => {
-  const tenant_id = mongoose.Types.ObjectId(req.query.tenant_id);
+  co(function* () {
+    try {
+      const { tenant_id } = req.query;
+      if (tenant_id === undefined ||
+          tenant_id === null ||
+          tenant_id === "") throw "tenant_id is empty";
 
-  MetaInfo.find({ tenant_id: tenant_id })
-    .then( meta => {
+      const meta_infos = yield MetaInfo.find({
+        tenant_id: mongoose.Types.ObjectId(tenant_id)
+      });
+
       res.json({
         status: { success: true },
-        body: meta
+        body: meta_infos
       });
-    })
-    .catch( err => {
-      res.status(500).json({
-        status: { success: false, errors: err}
+    }
+    catch (e) {
+      let errors = {};
+
+      switch (e) {
+      case "tenant_id is empty":
+        errors.tenant_id = e;
+        break;
+      default:
+        errors.unknown = e;
+        break;
+      }
+
+      res.status(400).json({
+        status: { success: false, errors }
       });
-    });
+    }
+  });
 });
 
 export default router;
