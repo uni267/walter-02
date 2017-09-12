5ea5ac7bf6eea8f0f984e32c4bf4a3e01bbdef63
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-09-12 19:17:31 +0900

    promise -> generator

--- server/lib/routes/tenants.js
+++ server/lib/routes/tenants.js
@@ -1,25 +1,46 @@
 import { Router } from "express";
+import co from "co";
 import mongoose from "mongoose";
 
 import Tenant from "../models/Tenant";
 const router = Router();
 
 // view
-router.get("/:id", (req, res, next) => {
-  console.log(req.params.id);
+router.get("/:tenant_id", (req, res, next) => {
+  co(function* () {
+    try {
+      const { tenant_id } = req.params;
 
-  Tenant.findById(req.params.id, (err, tenant) => {
-    if (tenant === undefined) {
+      if (tenant_id === null ||
+          tenant_id === undefined ||
+          tenant_id === "") throw "tenant_id is empty";
+
+      const tenant = yield Tenant.findById(tenant_id);
+      if (tenant === null) throw "tenant is empty";
+
       res.json({
-        status: { success: true, message: "テナントが存在しません" },
-        body: {}
+        status: { success: true },
+        body: tenant
       });
     }
-  }).then(tenant => {
-    res.json({
-      status: { success: true },
-      body: tenant
-    });
+    catch (e) {
+      let errors = {};
+      switch (e) {
+      case "tenant_id is empty":
+        errors.tenant_id = e;
+        break;
+      case "tenant is empty":
+        errors.tenant = e;
+        break;
+      default:
+        errors.unknown = e;
+        break;        
+      }
+
+      res.status(400).json({
+        status: { success: false, errors }
+      });
+    }
   });
 });
 
