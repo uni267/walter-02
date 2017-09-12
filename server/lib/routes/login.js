f0ebead8d7bb4e368446c89b2160ceff1da890d6
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-09-12 17:55:55 +0900

    promise -> generator

--- server/lib/routes/login.js
+++ server/lib/routes/login.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import co from "co";
 import crypto from "crypto";
 import jwt from "jsonwebtoken";
 import { SECURITY_CONF } from "../../configs/server";
@@ -8,16 +9,20 @@ import Tenant from "../models/Tenant";
 const router = Router();
 
 router.post("/", (req, res, next) => {
+  co(function* () {
+    try {
+      const { email, password } = req.body;
 
-  User.findOne({ email: req.body.email })
-    .then( user => {
-      if (user === null) throw "user not found";
-      return user;
-    })
-    .then( user => {
+      if (email === undefined ||
+          email === null ||
+          email === "") throw "email is empty";
+
+      const user = yield User.findOne({ email });
+
+      if (user === null) throw "user is empty";
 
       const sha = crypto.createHash("sha512");
-      sha.update(req.body.password);
+      sha.update(password);
       const hash = sha.digest("hex");
 
       if (user.password !== hash) throw "password is invalid";
@@ -25,48 +30,39 @@ router.post("/", (req, res, next) => {
       const { secretKey } = SECURITY_CONF.development;
       const token = jwt.sign(user, secretKey, { expiresIn: "24h" });
 
-      res.token = token;
-      return user;
-    })
-    .then( user => {
-      res.user = user.toObject();
-
-      return Tenant.findOne(user.tenant_id);
-    })
-    .then( tenant => {
-      res.user.tenant = tenant;
+      const tenant = yield Tenant.findOne(user.tenant_id);
 
       res.json({
-        status: { success: true, message: "ログインに成功しました" },
+        status: { success: true },
         body: {
-          token: res.token,
-          user: res.user
+          token,
+          user: {...user.toObject(), tenant: tenant.toObject() }
         }
       });
-    })
-    .catch( err => {
-      let errors;
+    }
+    catch (e) {
+      let errors = {};
 
-      switch ( err ) {
-      case "user not found":
-        errors = { name: "ユーザが存在しません" };
+      switch (e) {
+      case "email is empty":
+        errors.name = "ユーザ名が空です";
+        break;
+      case "user is empty":
+        errors.name = "ユーザが存在しません";
         break;
       case "password is invalid":
-        errors = { password: "パスワードに誤りがあります" };
+        errors.password = "パスワードに誤りがあります";
         break;
       default:
-        errors = err;
+        console.log(e);
+        errors.unknown = e;
         break;
       }
-
       res.status(400).json({
-        status: {
-          success: false,
-          message: "認証に失敗しました",
-          errors: errors
-        }
+        status: { success: false, errors }
       });
-    });
+    }
+  });
 });
 
 export default router;
