a490e0809b5f432c51ea7b931c4affcf634f1ff1
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-09-15 16:31:43 +0900

    refactor

--- client/src/sagas/watchMoveFile.js
+++ client/src/sagas/watchMoveFile.js
@@ -3,20 +3,20 @@ import { call, put, take } from "redux-saga/effects";
 
 import { API } from "../apis";
 
+import * as actions from "../actions";
+
 function* watchMoveFile() {
   while (true) {
-    const { dir, file } = yield take("MOVE_FILE");
-    yield put({ type: "LOADING_START" });
-    yield call(delay, 1000);
+    const task = yield take(actions.moveFile().type);
+    const { dir, file } = task;
+    yield put(actions.loadingStart());
 
     try {
+      yield call(delay, 1000);
       yield call(API.moveFile, dir, file);
       const payload = yield call(API.fetchFiles, file.dir_id);
-      yield put({ type: "INIT_FILES", files: payload.data.body });
-      yield put({
-        type: "TRIGGER_SNACK",
-        message: `${file.name}を${dir.name}に移動しました`
-      });
+      yield put(actions.initFiles(payload.data.body));
+      yield put(actions.triggerSnackbar(`${file.name}を${dir.name}に移動しました`));
     }
     catch (e) {
       console.log(e);
