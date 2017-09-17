573790d413958c768f76b68b8880fd20867fd08f
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-09-17 15:46:22 +0900

    [refactor] add controller layer

--- server/lib/routes/metaInfos.js
+++ server/lib/routes/metaInfos.js
@@ -1,44 +1,7 @@
-import mongoose from "mongoose";
-import co from "co";
 import { Router } from "express";
-import MetaInfo from "../models/MetaInfo";
-
+import * as controllers from "../controllers";
 const router = Router();
 
-router.get("/", (req, res, next) => {
-  co(function* () {
-    try {
-      const { tenant_id } = req.query;
-      if (tenant_id === undefined ||
-          tenant_id === null ||
-          tenant_id === "") throw "tenant_id is empty";
-
-      const meta_infos = yield MetaInfo.find({
-        tenant_id: mongoose.Types.ObjectId(tenant_id)
-      });
-
-      res.json({
-        status: { success: true },
-        body: meta_infos
-      });
-    }
-    catch (e) {
-      let errors = {};
-
-      switch (e) {
-      case "tenant_id is empty":
-        errors.tenant_id = e;
-        break;
-      default:
-        errors.unknown = e;
-        break;
-      }
-
-      res.status(400).json({
-        status: { success: false, errors }
-      });
-    }
-  });
-});
+router.get("/", controllers.index);
 
 export default router;
