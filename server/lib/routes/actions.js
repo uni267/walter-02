528d7d06fa67798b6e35c7692cc59c49168c7618
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-09-18 11:24:46 +0900

    [refactor] add controller layer

--- server/lib/routes/actions.js
+++ server/lib/routes/actions.js
@@ -1,35 +1,10 @@
 import { Router } from "express";
-import mongoose from "mongoose";
-import co from "co";
-import Action from "../models/Action";
+import * as controller from "../controllers/actions";
 
 const router = Router();
 
 // 一覧
-router.get("/", (req, res,next) => {
-  co(function* () {
-    try {
-      const actions = yield Action.find();
-
-      res.json({
-        status: { success: true },
-        body: actions
-      });
-    }
-    catch (e) {
-      let errors = {};
-
-      switch (e) {
-      default:
-        errors.unknown = e;
-        break;
-      }
-
-      res.status(400).json({
-        status: { success: false, errors }
-      });
-    }
-  });
-});
+router.route("/")
+  .get(controller.index);
 
 export default router;
