c87c222d499406efdd7b3d9e2bec733dfa8c7e79
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-09-19 12:59:30 +0900

    buildした静的js対応

--- server/lib/index.js
+++ server/lib/index.js
@@ -21,6 +21,7 @@ app.use( (req, res, next) => {
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(morgan({ format: "dev", immediate: true }));
+app.use("/", express.static("build"));
 
 // 環境変数
 // 開発 => development、社内テスト => integration、本番 => production
