93f0a7dbf13c6f2d26244a854e1c733b56b9deb4
KouheiOshima <k-oshima@cyber-l.co.jp>
2017-10-04 09:39:48 +0900

    メタ情報追加api

--- server/lib/controllers/metaInfos.js
+++ server/lib/controllers/metaInfos.js
@@ -45,3 +45,59 @@ export const index = (req, res, next) => {
     }
   });
 };
+
+export const add = (req, res, next) => {
+  co(function*() {
+    try{
+
+      const metainfo = new MetaInfo(req.body);
+
+      const { tenant_id } = res.user;
+
+      if (tenant_id === undefined ||
+          tenant_id === null ||
+          tenant_id === "") throw "tenant_id is empty";
+
+      if (metainfo.key === undefined ||
+          metainfo.key === null ||
+          metainfo.key === "") throw "key is empty";
+
+      if (metainfo.value_type === undefined ||
+          metainfo.value_type === null ||
+          metainfo.value_type === "") throw "value_type is empty";
+
+      metainfo.key_type = "meta";
+      metainfo.tenant_id = tenant_id;
+
+      let _metainfo = yield MetaInfo.findOne({ key: metainfo.key });
+      if(_metainfo !== null) throw "key is duplicate";
+     
+      const createdMetainfo = yield metainfo.save();
+
+      res.json({
+        status: { success: true},
+        body: createdMetainfo
+      });      
+    }catch(err){
+      let errors = {};
+      switch (err) {
+        case "key is empty":
+          errors.name = "表示名が空です";
+          break;
+        case "key is duplicate":
+          errors.name = "既に同名の値が存在します";
+          break;
+        case "value_type is empty":
+          errors.name = "型が空です";
+          break;
+        default:
+          errors = err;
+          break;
+      }
+      res.status(400).json({
+        status: { success: false, errors }
+      });
+    }
+
+  });
+}
--- server/lib/routes/metaInfos.js
+++ server/lib/routes/metaInfos.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import * as controllers from "../controllers/metaInfos";
 const router = Router();
 
-router.get("/", controllers.index);
+router.route("/")
+  .get(controllers.index) // 一覧
+  .post(controllers.add); // 作成
 
 export default router;
