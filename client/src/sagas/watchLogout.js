a3c1e8cf82e8dd144a78133719e81d66a78aff90
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-10-05 18:22:03 +0900

    ログアウト後、snackbarが都度表示される不具合をfix

--- client/src/sagas/watchLogout.js
+++ client/src/sagas/watchLogout.js
@@ -1,12 +1,18 @@
-import { put, take } from "redux-saga/effects";
+import { delay } from "redux-saga";
+import { call, put, take } from "redux-saga/effects";
+
+import * as actions from "../actions";
+import * as actionTypes from "../actionTypes";
 
 function* watchLogout() {
   while (true) {
-    yield take("LOGOUT");
+    yield take(actionTypes.LOGOUT);
     localStorage.removeItem("token");
 
-    yield put({ type: "LOGOUT" });
-    yield put({ type: "TRIGGER_SNACK", message: "ログアウトしました" });
+    yield put(actions.logout());
+    yield put(actions.triggerSnackbar("ログアウトしました"));
+    yield call(delay, 3000);
+    yield put(actions.closeSnackbar());
   }
 }
 
