26b6079108702b8896aa6e664047f96562bea509
y_wakayama <konbu.wakayama@gmail.com>
2017-10-11 18:57:58 +0900

    ユーザマスタ系のアクションを切り出し

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -224,22 +224,6 @@ export const updateMetaInfoTarget = (target_file) => ({
   type: actionTypes.UPDATE_META_INFO_TARGET, target_file
 });
 
-export const requestFetchUsers = () => ({
-  type: actionTypes.REQUEST_FETCH_USERS
-});
-
-export const requestFetchUser = (user_id, tenant_id) => ({
-  type: actionTypes.REQUEST_FETCH_USER, user_id, tenant_id
-});
-
-export const initUsers = (users) => ({
-  type: actionTypes.INIT_USERS, users
-});
-
-export const initUser = (user) => ({
-  type: actionTypes.INIT_USER, user
-});
-
 export const initGroups = (groups) => ({
   type: actionTypes.INIT_GROUPS, groups
 });
@@ -256,24 +240,12 @@ export const addGroupOfUser = (user_id, group_id) => ({
   type: actionTypes.ADD_GROUP_OF_USER, user_id, group_id
 });
 
-export const toggleUser = (user_id) => ({
-  type: actionTypes.TOGGLE_USER, user_id
-});
-
 export const changeUserName = (name) => ({
   type: actionTypes.CHANGE_USER_NAME, name
 });
 
 export const changeUserAccountName = (account_name) => ({
   type: actionTypes.CHANGE_USER_ACCOUNT_NAME, account_name
-});
-
-export const changeUserValidationError = (errors) => ({
-  type: actionTypes.CHANGE_USER_VALIDATION_ERROR, errors
-});
-
-export const clearUserValidationError = () => ({
-  type: actionTypes.CLEAR_USER_VALIDATION_ERROR
 });
 
 export const changeUserEmail = (email) => ({
@@ -284,28 +256,12 @@ export const changeUserPassword = (password) => ({
   type: actionTypes.CHANGE_USER_PASSWORD, password
 });
 
-export const saveUserName = (user) => ({
-  type: actionTypes.SAVE_USER_NAME, user
-});
-
-export const saveUserAccountName = (user) => ({
-  type: actionTypes.SAVE_USER_ACCOUNT_NAME, user
-});
-
-export const saveUserEmail = (user) => ({
-  type: actionTypes.SAVE_USER_EMAIL, user
-});
-
 export const saveUserPassword = (user) => ({
   type: actionTypes.SAVE_USER_PASSWORD, user
 });
 
-export const saveUserPasswordForce = (user) => ({
-  type: actionTypes.SAVE_USER_PASSWORD_FORCE, user
-});
-
-export const searchUsersSimple = (tenant_id, keyword) => ({
-  type: actionTypes.SEARCH_USERS, tenant_id, keyword
+export const requestFetchUsers = () => ({
+  type: actionTypes.REQUEST_FETCH_USERS
 });
 
 export const requestFetchGroups = (tenant_id) => ({
@@ -342,10 +298,6 @@ export const clearGroupValidationError = () => ({
 
 export const initNewUserTemplate = () => ({
   type: actionTypes.INIT_NEW_USER_TEMPLATE
-});
-
-export const createUser = (user, history) => ({
-  type: actionTypes.CREATE_USER, user, history
 });
 
 export const createGroup = (group, history) => ({
