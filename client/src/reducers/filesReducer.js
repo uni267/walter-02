a7630a9c5c59be1c2715f57e9120017d599dc5fe
y_wakayama <konbu.wakayama@gmail.com>
2017-10-17 09:44:58 +0900

    不要なアクション

--- client/src/reducers/filesReducer.js
+++ client/src/reducers/filesReducer.js
@@ -20,35 +20,6 @@ const filesReducer = (state = [], action) => {
       ? state.map( file => ({ ...file, checked: true }) )
       : state.map( file => ({ ...file, checked: false }) );
 
-  case actionTypes.SORT_FILE:
-    let _state = state.slice();
-
-    _state.sort( (a, b) => {
-      if (action.desc) {
-        return a[action.sorted] > b[action.sorted];
-      } else {
-        return a[action.sorted] < b[action.sorted];
-      }
-    });
-    return _state;
-    
-  case actionTypes.DELETE_AUTHORITY:
-    return state.map(file => {
-      if (Number(file.id) === Number(action.file_id)) {
-        const authorities = file.authorities
-              .filter(auth => Number(auth.id) !== Number(action.authority_id));
-
-        return {...file, authorities};
-      }
-      return file;
-    });
-
-  case actionTypes.COPY_FILE:
-    const nextFileId = state.slice().sort((a,b) => b.id - a.id)[0].id + 1;
-    let file = {...action.file, dir_id: action.dir_id};
-    file = {...file, id: nextFileId};
-    return [...state, file];
-
   default:
     return state;
   }
