e9c4c3c946af48aa3e2798e4eca3d3ba7cd73e38
y_wakayama <konbu.wakayama@gmail.com>
2017-10-17 18:55:44 +0900

    ごみ箱を経由せず削除される不具合をfix

--- client/src/sagas/watchDeleteFiles.js
+++ client/src/sagas/watchDeleteFiles.js
@@ -8,21 +8,34 @@ import * as commons from "../actions/commons";
 
 function* watchDeleteFiles() {
   while (true) {
-    const task = yield take(actions.deleteFiles().type);
+    const { files } = yield take(actions.deleteFiles().type);
     const api = new API();
     yield put(commons.loadingStart());
-    
+    yield call(delay, 1000);
+
     try {
-      yield call(delay, 1000);
-      const trashDirId = yield select( state => state.tenant.trashDirId );
-      const jobs = task.files.map( file => call(api.deleteFile, file, trashDirId) );
-      yield all(jobs);
-      const payload = yield call(api.fetchFiles, task.files[0].dir_id, 0);
+      const tenant = yield select( state => state.tenant );
+      let message;
+
+      if ( files[0].dir_id === tenant.trashDirId ) {
+        const tasks = files.map ( file => call(api.deleteFile, file) );
+        yield all(tasks);
+        message = "ファイルをごみ箱から削除しました";
+      }
+      else {
+        const tasks = files.map( file => call(api.moveFileTrash, file) );
+        yield all(tasks);
+        message = "ファイルをごみ箱へ移動しました";
+      }
+
+      const payload = yield call(api.fetchFiles, files[0].dir_id, 0);
       yield put(actions.initFiles(payload.data.body));
       yield put(actions.initFileTotal(payload.data.status.total));
       yield put(actions.toggleDeleteFilesDialog());
+      yield put(commons.triggerSnackbar(message));
     }
     catch (e) {
+      console.log(e);
     }
     finally {
       yield put(commons.loadingEnd());
