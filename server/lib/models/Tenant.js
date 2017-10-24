d742f587b877f2f157c1a1c8ac4001c181bb2cfb
y_wakayama <konbu.wakayama@gmail.com>
2017-10-24 15:16:42 +0900

    テナントに制限容量の閾値を追加

--- server/lib/models/Tenant.js
+++ server/lib/models/Tenant.js
@@ -5,7 +5,8 @@ mongoose.Promise = global.Promise;
 const TenantSchema = Schema({
   name: String,
   home_dir_id: Schema.Types.ObjectId,
-  trash_dir_id: Schema.Types.ObjectId
+  trash_dir_id: Schema.Types.ObjectId,
+  threshold: Schema.Types.Number
 });
 
 const Tenant = mongoose.model("tenants", TenantSchema, "tenants");
