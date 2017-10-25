aaaec844a2c36e6aca763bc1bb5a35adabec9f64
y_wakayama <konbu.wakayama@gmail.com>
2017-10-25 17:40:44 +0900

    容量管理

--- server/lib/controllers/analysis.js
+++ server/lib/controllers/analysis.js
@@ -40,33 +40,45 @@ export const index = (req, res, next) => {
         AnalysisUseRateMimeType.find(conditions)
       ];
 
-      useRateTotal = [
-        { name: "usage", label: "使用容量", value: useRateTotal[0].used },
-        { name: "free", label: "空き容量", value: useRateTotal[0].free }
-      ];
+      if (useRateTotal.length > 0) {
+        useRateTotal = [
+          { name: "usage", label: "使用容量", value: useRateTotal[0].used },
+          { name: "free", label: "空き容量", value: useRateTotal[0].free }
+        ];
+      }
 
-      fileCount = [{
-        name: "fileCount", value: fileCount[0].count
-      }];
+      if (fileCount.length > 0) {
+        fileCount = [{
+          name: "fileCount", value: fileCount[0].count
+        }];
+      }
 
-      folderCount = [{
-        name: "folderCount", value: folderCount[0].count
-      }];
+      if (folderCount.length > 0) {
+        folderCount = [{
+          name: "folderCount", value: folderCount[0].count
+        }];
+      }
 
-      useRateFolder = useRateFolder.map( f => ({
-        name: f.dir_name, value: f.rate
-      }));
+      if (useRateFolder.length > 0) {
+        useRateFolder = useRateFolder.map( f => ({
+          name: f.dir_name, value: f.rate
+        }));
+      }
 
-      useRateTag = useRateTag.map( t => {
-        if (t.tag_label === null) {
-          t.tag_label = "タグなし";
-        }
-        return { name: t.tag_label, value: t.rate };
-      });
+      if (useRateTag.length > 0) {
+        useRateTag = useRateTag.map( t => {
+          if (t.tag_label === null) {
+            t.tag_label = "タグなし";
+          }
+          return { name: t.tag_label, value: t.rate };
+        });
+      }
 
-      useRateMimeType = useRateMimeType.map( m => ({
-        name: m.mime_type, value: m.rate
-      }));
+      if (useRateMimeType.length > 0) {
+        useRateMimeType = useRateMimeType.map( m => ({
+          name: m.mime_type, value: m.rate
+        }));
+      }
 
       res.json({
         status: { success: true },
