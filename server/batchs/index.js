7c593f8832a2ee578b3e6e34283520534ca69df8
y_wakayama <konbu.wakayama@gmail.com>
2017-10-30 15:58:48 +0900

    バッチはweb用のポートを利用しないので削除

--- server/batchs/index.js
+++ server/batchs/index.js
@@ -12,26 +12,22 @@ const mode = process.env.NODE_ENV;
 
 let url;
 let db_name;
-let port;
 
 switch (mode) {
 
 case "integration":
   url = SERVER_CONF.integration.url;
   db_name = SERVER_CONF.integration.db_name;
-  port = SERVER_CONF.integration.port;
   break;
 
 case "production":
   url = SERVER_CONF.production.url;
   db_name = SERVER_CONF.production.db_name;
-  port = SERVER_CONF.production.port;
   break;
 
 default:
   url = SERVER_CONF.development.url;
   db_name = SERVER_CONF.development.db_name;
-  port = SERVER_CONF.development.port;
   break;
 }
 
