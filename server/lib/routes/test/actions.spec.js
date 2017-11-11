df211f6f34409e7a2ff6b44b1d0c12e5ea6abe07
y_wakayama <konbu.wakayama@gmail.com>
2017-11-11 17:23:51 +0900

    routerは一括でimportしたほうが都合がよい & token対応

--- server/lib/routes/test/actions.spec.js
+++ server/lib/routes/test/actions.spec.js
@@ -1,18 +1,28 @@
 import request from "supertest";
 import { expect } from "chai";
 import mongoose from "mongoose";
-import ActionRouter from "../actions";
+import Router from "../";
 import { intersection, uniq } from "lodash";
-import { app, mongoUrl, initdbPromise } from "./builder";
+import { app, mongoUrl, initdbPromise, authData } from "./builder";
 
 const base_url = "/api/v1/actions";
+const login_url = "/api/login";
+let auth;
 
 describe(base_url, () => {
-  app.use(base_url, ActionRouter);
+  app.use("/", Router);
   mongoose.connect(mongoUrl, { useMongoClient: true });
 
   before ( done => {
-    initdbPromise.then( () => done() );
+    initdbPromise.then( () => {
+      request(app)
+        .post(login_url)
+        .send(authData)
+        .end( (err, res) => {
+          auth = res.body.body;
+          done();
+        });
+    });
   });
 
   describe("get /", () => {
@@ -23,6 +33,7 @@ describe(base_url, () => {
     it("http(200)が返却される", done => {
       request(app)
         .get(base_url)
+        .set("x-auth-cloud-storage", auth.token)
         .expect(200)
         .end( ( err, res ) => {
           done();
@@ -32,6 +43,7 @@ describe(base_url, () => {
     it(`概要は「${expected.message}」`, done => {
       request(app)
         .get(base_url)
+        .set("x-auth-cloud-storage", auth.token)
         .end( (err, res) => {
           expect(res.body.status.message).equal(expected.message);
           done();
@@ -41,6 +53,7 @@ describe(base_url, () => {
     it("10件以上のオブジェクトが返却される", done => {
       request(app)
         .get(base_url)
+        .set("x-auth-cloud-storage", auth.token)
         .end( (err, res) => {
           expect(res.body.body.length > 10).equal(true);
           done();
@@ -50,6 +63,7 @@ describe(base_url, () => {
     it("返却されるオブジェクトは_id, name, labelカラムを含んでいる", done => {
       request(app)
         .get(base_url)
+        .set("x-auth-cloud-storage", auth.token)
         .end( (err, res) => {
           const needle = ["_id", "name", "label"];
 
@@ -65,6 +79,7 @@ describe(base_url, () => {
     it("返却されるオブジェクトの_id, name, labelカラムは0文字以上", done => {
       request(app)
         .get(base_url)
+        .set("x-auth-cloud-storage", auth.token)
         .end( (err, res) => {
           const needle = ["_id", "name", "label"];
 
@@ -82,6 +97,7 @@ describe(base_url, () => {
     it("nameカラムはuniqueなもの", done => {
       request(app)
         .get(base_url)
+        .set("x-auth-cloud-storage", auth.token)
         .end( (err, res) => {
           const names = res.body.body.map( obj => obj.name );
           expect( uniq(names).length ).equal(names.length);
