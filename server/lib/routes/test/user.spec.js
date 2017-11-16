fb16926e4657ad4a2a99c17c335d70d95106e343
knb-wkm <konbu.wakayama@gmail.com>
2017-11-16 09:42:11 +0900

    /users/:user_idのget

--- server/lib/routes/test/user.spec.js
+++ server/lib/routes/test/user.spec.js
@@ -3,74 +3,127 @@ import defaults from "superagent-defaults";
 import { expect } from "chai";
 import mongoose from "mongoose";
 import Router from "../";
-import {
-  intersection,
-  uniq,
-  head,
-  last,
-  includes,
-  has,
-  pick,
-  keys,
-  values,
-  chain,
-  range
-} from "lodash";
+import { intersection, uniq, head, last, includes, has, pick, keys, values, chain, range } from "lodash";
 import { app, mongoUrl, initdbPromise, authData } from "./builder";
 
 mongoose.connect(mongoUrl, { useMongoClient: true });
 app.use("/", Router);
 
-const user_url = "/api/v1/users/:user_id";
+const user_url = "/api/v1/users";
 const login_url = "/api/login";
 
 const request = defaults(supertest(app));
 let user;
 
 // ユーザ詳細
-describe(user_url, () => {
+describe(user_url + "/:user_id", () => {
+  before( done => {
+    initdbPromise.then( () => {
+      request.post(login_url)
+        .send(authData)
+        .end( (err, res) => {
+          request.set("x-auth-cloud-storage", res.body.body.token);
+          user = res.body.body.user;
+          done();
+        });
+    });
+  });
+
   describe("get /", () => {
     describe("ログインユーザのuser_idを指定した場合", () => {
-      it("http(200)が返却される");
-      it("1個のオブジェクトが返却される");
+      let payload;
+
+      before( done => {
+        request
+          .get(user_url + `/${user._id}`)
+          .end( (err, res) => {
+            payload = res;
+            done();
+          });
+      });
+
+      it("http(200)が返却される", done => {
+        expect(payload.status).equal(200);
+        done();
+      });
+
+      it("1個のオブジェクトが返却される", done => {
+        expect(typeof payload.body.body === "object").equal(true);
+        done();
+      });
 
       describe("userオブジェクトの型", () => {
-        it("_id, name, account_name, emailが含まれている");
-        it("passwordが含まれていない");
-        it("groupsが含まれている");
-        it("groups[0]に_id, name, description, tenant_idが含まれている");
-        it("tenant, tenant_idが含まれている");
-        it("tenantオブジェクトに_id, name, home_dir_id, trash_dir_id, thresholdが含まれている");
+        it("_id, name, account_name, email, tenant_idが含まれている", done => {
+          const needle = ["_id", "name", "account_name", "email", "tenant_id"];
+          expect(
+            chain(payload.body.body).pick(needle).keys().value().length === needle.length
+          ).equal(true);
+
+          done();
+        });
+
+        it("groupsが含まれている", done => {
+          const needle = ["groups"];
+          expect(
+            chain(payload.body.body).pick(needle).keys().value().length === needle.length
+          ).equal(true);
+          done();
+        });
+
+        it("groups[0]に_id, name, description, tenant_idが含まれている", done => {
+          const needle = ["_id", "name", "description", "tenant_id"];
+          const columns = chain(payload.body.body.groups).head().pick(needle).keys().value();
+          expect(columns.length === needle.length).equal(true);
+          done();
+        });
+
+        it("groups[0].tenantにはname, role_files, tenant_id, rolesが含まれている", done => {
+          const needle = ["name", "role_files", "tenant_id", "roles"];
+          const tenant = chain(payload.body.body.groups).head().value();
+          const columns = chain(tenant).pick(needle).keys().value();
+          expect(columns.length === needle.length).equal(true);
+          done();
+        });
       });
     });
 
-    describe("指定されたuser_idが", () => {
-      describe("undefinedの場合", () => {
-        it("http(400)が返却される");
-        it("statusはfalse");
-        it("エラーの概要は「xx」");
-        it("エラーの詳細は「xx」");
-      });
-
-      describe("nullの場合", () => {
-        it("http(400)が返却される");
-        it("statusはfalse");
-        it("エラーの概要は「xx」");
-        it("エラーの詳細は「xx」");
-      });
-
-      describe("空文字の場合", () => {
-        it("http(400)が返却される");
-        it("statusはfalse");
-        it("エラーの概要は「xx」");
-        it("エラーの詳細は「xx」");
-      });
-
+    describe.only("指定されたuser_idが", () => {
       describe("存在しないoidの場合", () => {
-        it("http(400)が返却される");
-        it("statusはfalse");
-        it("エラーの概要は「xx」");
-        it("エラーの詳細は「xx」");
+        let payload;
+        let expected = {
+          message: "ユーザの取得に失敗しました",
+          detail: "指定されたユーザが存在しないためユーザの取得に失敗しました"
+        };
+
+        before( done => {
+          request
+            .get(user_url + "/undefined_oid")
+            .end( (err, res) => {
+              payload = res;
+              done();
+            });
+        });
+
+        it("http(400)が返却される", done => {
+          expect(payload.status).equal(400);
+          done();
+        });
+
+        it("statusはfalse", done => {
+          expect(payload.body.status.success).equal(false);
+          done();
+        });
+
+        it(`エラーの概要は「${expected.message}」`, done => {
+          expect(payload.body.status.message).equal(expected.message);
+          done();
+        });
+
+        it(`エラーの詳細は「${expected.detail}」`, done => {
+          expect(payload.body.status.errors.user_id).equal(expected.detail);
+          done();
+        });
+
       });
     });
   });
