e2b38cfcf827fbf6b25bb680332e212902e4928d
knb-wkm <konbu.wakayama@gmail.com>
2017-11-20 11:35:52 +0900

    routes/index.jsにてインポートしていなかったので

--- server/lib/routes/tenants.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as controller from "../controllers";
-
-const router = Router();
-
-// view
-router.get("/:tenant_id", controller.view);
-
-export default router;
