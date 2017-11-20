21d290cc299961f4b107a5c0faa31aa52f38b678
KouheiOshima <k-oshima@cyber-l.co.jp>
2017-11-20 17:48:20 +0900

    roleFilesのテストケースを追加

--- server/lib/controllers/roleFiles.js
+++ server/lib/controllers/roleFiles.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 import co from "co";
 import RoleFile from "../models/RoleFile";
 import Action from "../models/Action";
+import logger from "../logger";
 import * as commons from "./commons";
+import { findIndex } from "lodash";
+import { ValidationError, RecordNotFoundException } from "../errors/AppError";
+
+// constants
+import * as constants from "../../configs/constants";
 
 const { ObjectId } = mongoose.Types;
 
@@ -47,16 +53,22 @@ export const create = (req, res, next) => {
     try {
       const { role } = req.body;
 
+      if (role === undefined) throw new ValidationError( "role is empty" );
+
       if (role.name === undefined ||
           role.name === null ||
-          role.name === "") throw "name is empty";
+          role.name === "") throw new ValidationError( "name is empty" );
+
+      if ( role.name.length > constants.MAX_STRING_LENGTH) throw new ValidationError( "name is too long" );
+
+      if (role.description !== undefined &&  typeof role.description === "string" && role.description.length > constants.MAX_STRING_LENGTH) throw new ValidationError( "description is too long" );
 
       const _role = yield RoleFile.findOne({
         name: role.name,
         tenant_id: res.user.tenant_id
       });
 
-      if ( _role !== null ) throw "name is duplicate";
+      if ( _role !== null ) throw new ValidationError( "name is duplicate" );
 
       const newRoleFile = new RoleFile();
       newRoleFile.name = role.name;
@@ -73,20 +85,28 @@ export const create = (req, res, next) => {
     catch (e) {
       let errors = {};
 
-      switch (e) {
+      switch (e.message) {
+      case "role is empty":
+        errors.role = "ロールが空のため作成に失敗しました";
+        break;
       case "name is empty":
         errors.name = "ロール名が空のため作成に失敗しました";
         break;
       case "name is duplicate":
         errors.name = "同名のロールが既に存在するため作成に失敗しました";
         break;
+      case "name is too long":
+        errors.name = "ロール名が長過ぎるため作成に失敗しました";
+        break;
+      case "description is too long":
+        errors.description = "備考が長過ぎるため作成に失敗しました";
       default:
         errors.unknown = commons.errorParser(e);
         break;
       }
-
+      logger.error(e);
       res.status(400).json({
-        status: { success: false, errors }
+        status: { success: false,message:"ロールの作成に失敗しました", errors }
       });
     }
   });
@@ -96,8 +116,9 @@ export const view = (req, res, next) => {
   co(function* () {
     try {
       const { role_id } = req.params;
+      if( !ObjectId.isValid(role_id) ) throw new ValidationError( "role_id is not valid" );
       const role = yield RoleFile.findById(role_id);
-      if (role === null || role === undefined) throw "role is not found";
+      if (role === null || role === undefined) throw new ValidationError( "role is not found" );
 
       const actions = yield Action.find({ _id: { $in: role.actions } });
 
@@ -108,10 +129,19 @@ export const view = (req, res, next) => {
     }
     catch (e) {
       let errors = {};
-      errors = e;
+      switch (e.message){
+        case "role is not found":
+          errors.role = "ロールが存在しないため取得できませんでした";
+          break;
+        case "role_id is not valid":
+          errors.role = "ロールIDが不正です";
+          break;
+        default:
+          errors = e;
+      }
 
       res.status(400).json({
-        status: { success: false, errors }
+        status: { success: false, message:"ロールが取得できませんでした", errors }
       });
     }
 
@@ -123,10 +153,17 @@ export const updateName = (req, res, next) => {
     try {
       const { role_id } = req.params;
       const { name } = req.body;
-      if (name === undefined || name === null || name === "") throw "name is empty";
+      const { tenant_id } = res.user;
+      if (!ObjectId.isValid(role_id)) throw new ValidationError( "role_id is not valid" );
+
+      if (name === undefined || name === null || name === "") throw new ValidationError( "name is empty" );
+      if (name.length > constants.MAX_STRING_LENGTH) throw new ValidationError( "name is too long" );
+
+      const _role = yield RoleFile.find({name: name, tenant_id: tenant_id});
+      if(_role.length > 0) throw new ValidationError ( "name is duplicate" );
 
       const role = yield RoleFile.findById(role_id);
-      if (role === undefined || role === null) throw "role is not found";
+      if (role === undefined || role === null) throw new RecordNotFoundException( "role is not found" );
 
       role.name = name;
       const changedRoleFile = yield role.save();
@@ -139,9 +176,18 @@ export const updateName = (req, res, next) => {
     catch (e) {
       let errors = {};
 
-      switch (e) {
+      switch (e.message) {
       case "name is empty":
         errors.name = "名称が空のため変更に失敗しました";
+        break;
+      case "name is duplicate":
+        errors.name = "同名のロールが既に存在するため変更に失敗しました";
+        break;
+      case "name is too long":
+        errors.name = "名称が長過ぎるため変更に失敗しました";
+        break;
+      case "role_id is not valid":
+        errors.role_id = "ロールIDが不正です";
         break;
       case "role is not found":
         errors.role = "指定されたロールが見つからないため変更に失敗しました";
@@ -152,7 +198,7 @@ export const updateName = (req, res, next) => {
       }
 
       res.status(400).json({
-        status: { success: false, errors }
+        status: { success: false, message:"ロール名の変更に失敗しました", errors }
       });
     }
   });
@@ -164,8 +210,10 @@ export const updateDescription = (req, res, next) => {
       const { role_id } = req.params;
       const { description } = req.body;
 
+      if( !ObjectId.isValid(role_id) ) throw new ValidationError( "role_id is not valid" );
+
       const role = yield RoleFile.findById(role_id);
-      if (role === undefined || role === null) throw "role is not found";
+      if (role === undefined || role === null) throw new ValidationError( "role is not found" );
 
       role.description = description;
       const changedRoleFile = yield role.save();
@@ -178,17 +226,20 @@ export const updateDescription = (req, res, next) => {
     catch (e) {
       let errors = {};
 
-      switch (e) {
-      case "role is not found":
-        errors.role = "指定されたロールが見つからないため変更に失敗しました";
-        break;
+      switch (e.message) {
+        case "role is not found":
+          errors.role = "指定されたロールが見つからないため変更に失敗しました";
+          break;
+        case "role_id is not valid":
+          errors.role_id = "ロールIDが不正です";
+          break;
       default:
         errors.unknown = commons.errorParser(e);
         break;
       }
 
       res.status(400).json({
-        status: { success: false, errors }
+        status: { success: false, message:"備考の変更に失敗しました", errors }
       });
     }
   });
@@ -198,9 +249,10 @@ export const remove = (req, res, next) => {
   co(function* () {
     try {
       const { role_id } = req.params;
+      if( !ObjectId.isValid(role_id) ) throw new ValidationError( "role is not valid" );
       const role = yield RoleFile.findById(role_id);
 
-      if (role === null) throw "role is empty";
+      if (role === null) throw new ValidationError( "role is empty" );
 
       const deletedRoleFile = role.remove();
 
@@ -213,17 +265,20 @@ export const remove = (req, res, next) => {
     catch (e) {
       let errors = {};
 
-      switch (e) {
+      switch (e.message) {
       case "role is empty":
         errors.role = "指定されたロールが見つからないため削除に失敗しました";
+        break;
+      case "role is not valid":
+        errors.role = "ロールIDが不正です";
         break;
       default:
         errors.unknown = commons.errorParser(e);
         break;
       }
 
-      res.json({
-        status: { success: false, errors }
+      res.status(400).json({
+        status: { success: false, message:"ロールを削除できませんでした" ,errors }
       });
     }
   });
@@ -233,13 +288,17 @@ export const removeActionOfRoleFile = (req, res, next) => {
   co(function* () {
     try {
       const { role_id, action_id } = req.params;
+      if ( !ObjectId.isValid(role_id) ) throw new ValidationError( "role_id is not Valid" );
+      if ( !ObjectId.isValid(action_id) ) throw new ValidationError( "action_id is not Valid" );
+
       const [ role, action ] = yield [
         RoleFile.findById(role_id),
         Action.findById(action_id)
       ];
 
-      if (role === null) throw "role is empty";
-      if (action === null) throw "action is empty";
+      if (role === null) throw new RecordNotFoundException( "role is empty" );
+      if (action === null) throw new RecordNotFoundException( "action is empty" );
+      if (findIndex(role.actions, action._id) === -1) throw new ValidationError( "action is not found" );
 
       role.actions = role.actions.filter( _action => {
         return _action.toString() !== action._id.toString();
@@ -256,19 +315,28 @@ export const removeActionOfRoleFile = (req, res, next) => {
     catch (e) {
       let errors = {};
 
-      switch (e) {
-      case "role is empty":
-        errors.role = "指定されたロールが存在しないため削除に失敗しました";
-        break;
-      case "action is empty":
-        errors.action = "指定されたアクションが存在しないため削除に失敗しました";
-        break;
+      switch (e.message) {
+        case "role_id is not Valid":
+          errors.role_id = "ロールIDが不正です";
+          break;
+        case "action_id is not Valid":
+          errors.action_id = "アクションIDが不正です";
+          break;
+        case "role is empty":
+          errors.role = "指定されたロールが存在しないため削除に失敗しました";
+          break;
+        case "action is empty":
+          errors.action = "指定されたアクションが存在しないため削除に失敗しました";
+          break;
+        case "action is not found":
+          errors.action = "指定されたアクションは登録されていないため削除に失敗しました";
+          break;
       default:
         errors.unknown = commons.errorParser(e);
       }
-
+      logger.error(e);
       res.status(400).json({
-        status: { success: false, errors }
+        status: { success: false, message:"アクションの削除に失敗しました", errors }
       });
     }
   });
@@ -278,14 +346,17 @@ export const addActionToRoleFile = (req, res, next) => {
   co(function* () {
     try {
       const { role_id, action_id } = req.params;
+      if ( !ObjectId.isValid(role_id) ) throw new ValidationError( "role_id is not Valid" );
+      if ( !ObjectId.isValid(action_id) ) throw new ValidationError( "action_id is not Valid" );
+
       const [ role, action ] = yield [
         RoleFile.findById(role_id),
         Action.findById(action_id)
       ];
 
-      if (role === null) throw "role is empty";
-      if (action === null) throw "action is empty";
-      if (role.actions.indexOf(action._id) >= 0 ) throw "action is duplicate";
+      if (role === null) throw new RecordNotFoundException( "role is empty" );
+      if (action === null) throw new RecordNotFoundException( "action is empty" );
+      if (role.actions.indexOf(action._id) >= 0 ) throw new ValidationError( "action is duplicate" );
 
       role.actions = [ ...role.actions, action._id ];
       const changedRoleFile = yield role.save();
@@ -298,23 +369,30 @@ export const addActionToRoleFile = (req, res, next) => {
     }
     catch (e) {
       let errors = {};
-      switch (e) {
-      case "role is empty":
-        errors.role = "指定されたロールが見つからないためアクションの追加に失敗しました";
-        break;
-      case "action is empty":
-        errors.action = "指定されたアクションが見つからないため追加に失敗しました";
-        break;
-      case "action is duplicate":
-        errors.action = "指定されたアクションが既に登録されているため追加に失敗しました";
-        break;
-      default:
-        errors.unknown = commons.errorParser(e);
-        break;
+      switch (e.message) {
+        case "role_id is not Valid":
+          errors.role_id = "ロールIDが不正です";
+          break;
+        case "action_id is not Valid":
+          errors.action_id = "アクションIDが不正です";
+          break;
+        case "role is empty":
+          errors.role = "指定されたロールが見つからないためアクションの追加に失敗しました";
+          break;
+        case "action is empty":
+          errors.action = "指定されたアクションが見つからないため追加に失敗しました";
+          break;
+        case "action is duplicate":
+          errors.action = "指定されたアクションが既に登録されているため追加に失敗しました";
+          break;
+        default:
+          errors.unknown = commons.errorParser(e);
+          break;
       }
 
+      logger.error(e);
       res.status(400).json({
-        status: { success: false, errors }
+        status: { success: false, message:"アクションの追加に失敗しました", errors }
       });
     }
   });
