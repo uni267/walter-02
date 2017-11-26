c8b1be39977ed10c18d8660e5f63478b692223cb
knb-wkm <konbu.wakayama@gmail.com>
2017-11-27 08:55:32 +0900

    [refactor]

--- server/lib/routes/test/actions.spec.js
+++ server/lib/routes/test/actions.spec.js
@@ -29,76 +29,57 @@ describe(base_url, () => {
   });
 
   describe("get /", () => {
-    it("http(200)が返却される", done => {
+    let payload;
+
+    before( done => {
       request
         .get(base_url)
-        .end( ( err, res ) => {
-          expect(res.status).equal(200);
+        .end( (err, res) => {
+          payload = res;
           done();
         });
+    });
+    it("http(200)が返却される", done => {
+      expect(payload.status).equal(200);
+      done();
     });
 
     it("10件以上のオブジェクトが返却される", done => {
-      request
-        .get(base_url)
-        .end( (err, res) => {
-          expect(res.body.body.length > 10).equal(true);
-          done();
-        });
+      expect(payload.body.body.length > 10).equal(true);
+      done();
     });
 
     it("返却されるオブジェクトは_id, name, labelカラムを含んでいる", done => {
-      request
-        .get(base_url)
-        .end( (err, res) => {
-          const needle = ["_id", "name", "label"];
+      const needle = ["_id", "name", "label"];
 
-          const columns = res.body.body.map( o => (
-            intersection(Object.keys(o), needle).length === 3
-          ));
+      const columns = payload.body.body.map( o => (
+        intersection(Object.keys(o), needle).length === needle.length
+      ));
 
-          expect(columns.every( col => col === true )).equal(true);
-          done();
-        });
+      expect(columns.every( col => col === true )).equal(true);
+      done();
     });
 
     it("返却されるオブジェクトの_id, name, labelカラムは0文字以上", done => {
-      request
-        .get(base_url)
-        .end( (err, res) => {
-          const needle = ["_id", "name", "label"];
-
-          const columns = res.body.body.map( obj => {
-            return obj._id.length > 0 &&
-              obj.name.length > 0 &&
-              obj.label.length > 0;
-          });
-
-          expect(columns.every( col => col === true )).equal(true);
-          done();
-        });
+      const columns = payload.body.body.map( obj => {
+        return obj._id.length > 0 &&
+          obj.name.length > 0 &&
+          obj.label.length > 0;
+      });
+      expect(columns.every( col => col === true )).equal(true);
+      done();
     });
 
     it("nameカラムはuniqueなもの", done => {
-      request
-        .get(base_url)
-        .end( (err, res) => {
-          const names = res.body.body.map( obj => obj.name );
-          expect( uniq(names).length ).equal(names.length);
-          done();
-        });
+      const names = payload.body.body.map( obj => obj.name );
+      expect( uniq(names).length ).equal(names.length);
+      done();
     });
 
     it("labelカラムはuniqueなもの", done => {
-      request
-        .get(base_url)
-        .end( (err, res) => {
-          const labels = res.body.body.map( obj => obj.label );
-          expect( uniq(labels).length ).equal(labels.length);
-          done();
-        });
+      const labels = payload.body.body.map( obj => obj.label );
+      expect( uniq(labels).length ).equal(labels.length);
+      done();
     });
-
   });
-
 });
