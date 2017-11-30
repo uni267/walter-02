4ce7dc26520055afa63be6308721177611d32b5f
knb-wkm <konbu.wakayama@gmail.com>
2017-11-30 14:22:02 +0900

    詳細検索 date型

--- client/src/components/FileSearch/DetailSearch.js
+++ client/src/components/FileSearch/DetailSearch.js
@@ -15,46 +15,79 @@ class DetailSearch extends Component {
       items: []
     };
   }
-  
-  handleKeyPress = (event, item, searchValue) => {
-    if (event.key === "Enter") {
 
-      const queryItems = this.state.items.reduce( (pre, cur) => {
-        pre[cur._id] = cur.value;
-        return pre;
-      }, {});
+  execSearch = () => {
+    const queryItems = this.state.items.reduce( (prev, cur) => {
+      prev[cur._id] = cur.value;
+      return prev;
+    }, {});
 
-      this.props.actions.searchFileDetail(this.props.history, queryItems);
-
-    }
-    else {
-      const _item = { ...item, value: searchValue.getValue() };
-      const alreadyExists = this.state.items
-            .filter(item => item._id === _item._id).length > 0;
-
-      if (alreadyExists) {
-        const _items = this.state.items.map( item => {
-          return item._id === _item._id ? _item : item;
-        });
-        this.setState({ items: _items });
-      }
-      else {
-        this.setState({ items: [...this.state.items, _item ]});
-      }
-    }
+    this.props.actions.searchFileDetail(this.props.history, queryItems);
   };
 
-  searchField = (item) => {
+  appendSearchValue = (item, searchValue) => {
+    const _item = { ...item, value: searchValue };
+    const alreadyExists = this.state.items
+          .filter(item => item._id === _item._id).length > 0;
+
+    if (alreadyExists) {
+      const _items = this.state.items.map( item => (
+        item._id === _item._id ? _item : item
+      ));
+
+      this.setState({ items: _items });
+    }
+    else {
+      this.setState({ items: [...this.state.items, _item ] });
+    }
+  }
+
+  searchTextField = (item) => {
     let searchValue = "";
 
     return (
       <TextField
-        ref={(input) => searchValue = input}
-        onKeyPress={ e => this.handleKeyPress(e, item, searchValue) }
+        ref={(input) => searchValue = input }
+        onKeyPress={ e => {
+          if (e.key === "Enter") {
+            this.execSearch();
+          }
+          else {
+            this.appendSearchValue(item, searchValue.getValue() );
+          }
+        }}
         floatingLabelText={item.label}
         hintText={item.label}
         />
     );
+  };
+
+  searchDateField = (item) => {
+    return (
+      <DatePicker
+        onChange={ (e, value) => {
+          new Promise( (resolve, reject) => {
+            const result = this.appendSearchValue(item, value);
+            resolve(result);
+          }).then( res => {
+            this.execSearch();
+          });
+        }}
+        floatingLabelText={item.label}
+        hintText={item.label}
+        />
+    );
+  };
+
+  renderField = (item) => {
+    switch (item.value_type) {
+    case "String":
+      return this.searchTextField(item);
+    case "Date":
+      return this.searchDateField(item);
+    default:
+      return null;
+    }
   };
 
   renderForm = (item, idx) => {
@@ -66,7 +99,7 @@ class DetailSearch extends Component {
           <ContentRemoveCircleOutline />
         </IconButton>
 
-        {this.searchField(item)}
+        {this.renderField(item)}
       </div>
     );
   }
