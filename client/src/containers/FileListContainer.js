f3614a209b4f3deb501692f5635557ed0aac2aec
knb-wkm <konbu.wakayama@gmail.com>
2017-12-04 17:41:13 +0900

    意味が無いので...

--- client/src/containers/FileListContainer.js
+++ client/src/containers/FileListContainer.js
@@ -126,16 +126,11 @@ class FileListContainer extends Component {
   }
 
   componentWillMount() {
-    // @fixme
-    new Promise( (resolve, reject) => {
-      this.props.actions.requestFetchDisplayItems();
-      resolve();
-    }).then( res => {
-      this.props.actions.requestFetchFiles(this.props.match.params.id);
-      this.props.actions.requestFetchMetaInfos();
-      this.props.actions.requestFetchUsers();
-      this.props.actions.requestFetchRoles();
-    });
+    this.props.actions.requestFetchDisplayItems();
+    this.props.actions.requestFetchFiles(this.props.match.params.id);
+    this.props.actions.requestFetchMetaInfos();
+    this.props.actions.requestFetchUsers();
+    this.props.actions.requestFetchRoles();
   }
 
   componentDidMount() {
