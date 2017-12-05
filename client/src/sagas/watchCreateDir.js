5567e88a29fa6908ec1b323c978f5fdbdb64823f
knb-wkm <konbu.wakayama@gmail.com>
2017-12-05 15:39:31 +0900

    フォルダ作成時、dir_idが空だった場合はテナントのホーム

--- client/src/sagas/watchCreateDir.js
+++ client/src/sagas/watchCreateDir.js
@@ -1,14 +1,18 @@
-import { call, put, take } from "redux-saga/effects";
+import { call, put, take, select } from "redux-saga/effects";
 
 import { API } from "../apis";
 
 function* watchCreateDir() {
   while (true) {
-    const { dir_id, dir_name } = yield take("CREATE_DIR");
+    let { dir_id, dir_name } = yield take("CREATE_DIR");
     const api = new API();
     yield put({ type: "LOADING_START" });
 
     try {
+      if (dir_id === undefined || dir_id === null || dir_id === "") {
+        dir_id = yield select( state => state.tenant.dirId );
+      }
+
       yield call(api.createDir, dir_id, dir_name);
       const payload = yield call(api.fetchFiles, dir_id);
       yield put({ type: "INIT_FILES", files: payload.data.body });
