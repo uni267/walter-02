7726a745618dd767dd2bb18d1668df4bac7b8350
knb-wkm <konbu.wakayama@gmail.com>
2017-12-09 18:12:01 +0900

    デモデータ登録用のスクリプト

---
+++ server/jobs/demoData.rb
@@ -0,0 +1,135 @@
+# coding: utf-8
+require "open3"
+require "faraday"
+require "json"
+require "pp"
+require "date"
+
+require "./base64_data"
+
+# initdb
+dbhost = "172.16.55.78" # host決め打ち
+loadTestData = File.expand_path("loadTestData.js", File.dirname(__FILE__))
+cmd = "/usr/local/bin/mongo #{dbhost}/walter #{loadTestData}"
+
+o, e, s = Open3.capture3(cmd)
+raise "mongo loadTestData error" if (e != "")
+
+# 登録件数
+DATA_LENGTH = 100
+
+# 1requestあたりに何秒waitするか
+INTERVAL = 0.5
+
+# 1requestあたりに添付するファイル数
+POST_FILE_LENGTH = 20
+
+# post対象のサーバ
+API_SERVER = "http://localhost:3333"
+
+# どのユーザでpostするか
+ACCOUNT_INFO = {
+  account_name: "taro",
+  password: "test"
+}
+
+# tokkenを取得
+conn = Faraday.new( url: API_SERVER )
+payload = conn.post "/api/login", ACCOUNT_INFO
+body = JSON.parse(payload.body)
+
+token = body["body"]["token"]
+user = body["body"]["user"]
+
+# メタマスタを取得
+payload = conn.get do |req|
+  req.url "/api/v1/meta_infos"
+  req.headers["x-auth-cloud-storage"] = token
+end
+
+metainfos = JSON.parse(payload.body)["body"].select do |item|
+  needle = [
+    "send_date_time",
+    "receive_date_time",
+    "send_company_name",
+    "display_file_name"
+  ]
+    
+  needle.include? item["name"]
+end
+
+# タグマスタを取得
+payload = conn.get do |req|
+  req.url "/api/v1/tags"
+  req.headers["x-auth-cloud-storage"] = token
+end
+
+tag = JSON.parse(payload.body)["body"].first
+
+# 送受信日付の配列
+receive_date_range = (Date.parse("2017-11-01")..Date.parse("2017-12-20")).to_a
+send_date_range = (Date.parse("2017-10-01")..Date.parse("2017-11-01")).to_a
+
+# 送信企業名の配列
+send_company_names = [
+  ("a".."c").to_a.map{|s| (s * 3) + "グループ" },
+  ("d".."f").to_a.map{|s| (s * 3) + "ホールディングス"},
+  ("g".."i").to_a.map{|s| (s * 3) + "ジャパン"}
+].flatten
+
+# upload main
+(1..(DATA_LENGTH / POST_FILE_LENGTH)).each do |i|
+
+  files = POST_FILE_LENGTH.times.to_a.map do |j|
+    data = get_data
+
+    file = {
+      name: "#{i}_#{j}.pdf",
+      size: 167000,
+      mime_type: data[:mime_type],
+      base64: data[:base64],
+      checksum: data[:checksum]
+    }
+
+    send_date = send_date_range.shuffle.first
+    company_name = send_company_names.shuffle.first
+
+    metainfo = metainfos.map do |meta|
+      res = { "_id" => meta["_id"] }
+
+      case meta["name"]
+      when "receive_date_time"
+        res["value"] = receive_date_range.shuffle.first
+      when "send_date_time"
+        res["value"] = send_date
+      when "send_company_name"
+        res["value"] = company_name
+      when "display_file_name"
+        res["value"] = company_name + " " + send_date.strftime("%Y年%m月") + " 請求書"
+      else
+        raise "metainfo.name is invalid"
+      end
+      res
+    end
+
+    file[:meta_infos] = metainfo
+    file[:tags] = [ tag["_id"] ] if (0..10).to_a.shuffle.first === 0
+
+    # symbolを文字列に変換
+    file = file.map{ |k,v| [k.to_s, v] }.to_h
+    file
+  end
+
+  body = { "files" => files }
+
+  # upload
+  res = conn.post do |req|
+    req.url "/api/v1/files"
+    req.headers["Content-Type"] = "application/json"
+    req.headers["x-auth-cloud-storage"] = token
+    req.body = body.to_json
+  end
+
+  sleep(INTERVAL)
+
+end
