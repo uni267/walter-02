a8b220439b7e9d82f7ba94f31bddc2d55f47e7ba
KouheiOshima <k-oshima@cyber-l.co.jp>
2017-12-15 13:34:34 +0900

    [typo]

--- client/src/helper/dateTimeFormatter.js
+++ client/src/helper/dateTimeFormatter.js
@@ -1,8 +1,8 @@
 import * as moment from 'moment';
 
-const dateTimeFormatter = function(local,options){
+const dateTimeFormatter = function(locale,options){
   this.format = (datetime) => {
-    if(local === "ja") {
+    if(locale === "ja") {
       moment.locale('ja', {
         weekdays: ["日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"],
         weekdaysShort: ["日","月","火","水","木","金","土"],
