d938cdb3001b09e14dee1d7092e678841d08cd8c
KouheiOshima <k-oshima@cyber-l.co.jp>
2017-12-19 17:19:10 +0900

    IE対応

--- server/lib/index.js
+++ server/lib/index.js
@@ -19,6 +19,7 @@ app.use( (req, res, next) => {
   res.header("Access-Control-Allow-Headers",
              "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", true);
+  res.header("Cache-Control", "no-store");
   next();
 });
 
