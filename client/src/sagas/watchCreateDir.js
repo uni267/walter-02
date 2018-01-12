2c8443674e118910f030440445d1868f7b660dc4
KouheiOshima <k-oshima@cyber-l.co.jp>
2018-01-12 17:12:56 +0900

    追加したフォルダを先頭に追加する

--- client/src/sagas/watchCreateDir.js
+++ client/src/sagas/watchCreateDir.js
@@ -1,6 +1,9 @@
 import { call, put, take, select } from "redux-saga/effects";
 
 import { API } from "../apis";
+import * as commons from "../actions/commons";
+import * as filesAction from "../actions/files";
+import errorParser from "../helper/errorParser";
 
 function* watchCreateDir() {
   while (true) {
@@ -13,20 +16,29 @@ function* watchCreateDir() {
         dir_id = yield select( state => state.tenant.dirId );
       }
 
-      yield call(api.createDir, dir_id, dir_name);
-      const payload = yield call(api.fetchFiles, dir_id);
-      yield put({ type: "INIT_FILES", files: payload.data.body });
+      const payload = yield call(api.createDir, dir_id, dir_name);
+
+      // ファイル一覧の先頭に追加
+      const payloadFile = yield call(api.fetchFile, payload.data.body._id);
+      yield put(filesAction.insertFileRow([ payloadFile.data.body ]));
+
       yield put({ type: "TOGGLE_CREATE_DIR" });
       yield put({ type: "TRIGGER_SNACK", message: "フォルダを作成しました" });
     }
     catch (e) {
-      const { errors } = e.response.data.status;
-      yield put({ type: "CREATE_DIR_ERROR", errors });
+      const { message, errors } = errorParser(e,"フォルダの作成に失敗しました");
+      if(!errors.unknown){
+        yield put({ type: "CREATE_DIR_ERROR", errors });
+      }else{
+        yield put(commons.openException(message, errors.unknown ));
+      }
+
+
     }
     finally {
       yield put({ type: "LOADING_END" });
     }
-      
+
   }
 }
 
