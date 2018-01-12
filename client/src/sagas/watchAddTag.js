bffaff8e29fc5591fc7e6dadaac47bd2a065de9c
KouheiOshima <k-oshima@cyber-l.co.jp>
2018-01-12 15:36:00 +0900

    タグ追加処理後の一覧更新を再取得からstateの更新に変更

--- client/src/sagas/watchAddTag.js
+++ client/src/sagas/watchAddTag.js
@@ -3,6 +3,7 @@ import { call, put, take, select } from "redux-saga/effects";
 import { API } from "../apis";
 import * as actions from "../actions/files";
 import * as commons from "../actions/commons";
+import errorParser from "../helper/errorParser";
 
 function* watchAddTag() {
   while (true) {
@@ -13,24 +14,25 @@ function* watchAddTag() {
     try {
       yield call(api.fetchAddTag, file, tag);
 
-      // ソート、ページネーションの状態を保持する
-      const { sorted, desc } = yield select( state => state.fileSortTarget );
-      const { page } = yield select( state => state.filePagination );
-      const payload = yield call(api.fetchFiles, file.dir_id, page, sorted, desc);
-      yield put(actions.initFiles(payload.data.body));
+      const payload = yield call(api.fetchFile, file._id);
+      yield put(actions.updateFileRow(payload.data.body));
 
       // タグ編集ダイアログ用のfile objectをセット
-      const changedFile = payload.data.body.filter( _file => _file._id === file._id );
-      yield put(actions.initFileTag(changedFile[0]));
+      yield put(actions.initFileTag(payload.data.body));
       yield put(commons.triggerSnackbar("タグを追加しました"));
     }
     catch (e) {
-      console.log(e);
+      const { message, errors } = errorParser(e,"アクションの追加に失敗しました");
+      if(!errors.unknown){
+        yield put(commons.openException(message, errors.menu));
+      }else{
+        yield put(commons.openException(message, errors.unknown ));
+      }
     }
     finally {
       yield put(commons.loadingEnd());
     }
-      
+
   }
 }
 
