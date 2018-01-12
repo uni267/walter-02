fa233eefa667f7ca1ae4a96a6916e83736134bb7
knb-wkm <konbu.wakayama@gmail.com>
2018-01-12 10:28:27 +0900

    不要なので削除

--- server/lib/index.js
+++ server/lib/index.js
@@ -27,7 +27,6 @@ app.use( (req, res, next) => {
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({limit: '100mb'}));
 app.use(morgan({ format: "dev", immediate: true }));
-app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, '../../client/build')));
 app.use(log4js.connectLogger(logger));
 
