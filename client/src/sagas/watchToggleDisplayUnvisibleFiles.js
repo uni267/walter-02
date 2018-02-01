d34806696c2dbbde8626232e60521a135d57b643
knb-wkm <konbu.wakayama@gmail.com>
2018-02-01 13:57:33 +0900

    非表示ファイル 簡易検索画面、詳細検索画面

--- client/src/sagas/watchToggleDisplayUnvisibleFiles.js
+++ client/src/sagas/watchToggleDisplayUnvisibleFiles.js
@@ -1,10 +1,11 @@
 import { call, put, take, all, select } from "redux-saga/effects";
 import * as _ from "lodash";
 
+import { API } from "../apis";
 import * as actions from "../actions/files";
 import * as commons from "../actions/commons";
-import { fetchFiles } from "./watchFetchFiles";
 import errorParser from "../helper/errorParser";
+import { LIST_DEFAULT, LIST_SEARCH_SIMPLE, LIST_SEARCH_DETAIL } from "../constants";
 
 function* watchToggleDisplayUnvisibleFiles() {
   while(true) {
@@ -14,11 +15,50 @@ function* watchToggleDisplayUnvisibleFiles() {
 
     const dir = _.last(yield select( state => state.dirs ));
 
+    const dir_id = dir === undefined
+          ? (yield select( state => state.tenant )).dirId
+          : dir._id;
+
+    const api = new API();
+
+    // 非表示ファイルを取得するか
+    const isDisplayUnvisibleSetting = yield select( state => {
+      return state.appSettings.find( s => s.name === "unvisible_files_toggle" );
+    });
+
+    let isDisplayUnvisible;
+
+    if (isDisplayUnvisibleSetting) {
+      isDisplayUnvisible = isDisplayUnvisibleSetting.value;
+    } else {
+      const settingsPayload = yield call(api.fetchAppSettings);
+      const settings = settingsPayload.data.body;
+      isDisplayUnvisible = settings.find( s => s.name === "unvisible_files_toggle" ).default_value;
+    }
+
     try {
-      yield call(fetchFiles, dir._id, 0); // トグルした場合は1ページから表示するので
+      const { list_type } = yield select( state => state.fileListType);
+      const { sorted, desc } = yield select(state => state.fileSortTarget);
+
+      let payload;
+
+      switch (list_type) {
+      case LIST_SEARCH_DETAIL:
+        const { searchedItems } = yield select( state => state.fileDetailSearch );
+        payload = yield call(api.searchFilesDetail, searchedItems, 0, sorted, desc, isDisplayUnvisible);
+        break;
+      case LIST_SEARCH_SIMPLE:
+        const { value } = yield select( state => state.fileSimpleSearch.search_value );
+        payload = yield call(api.searchFiles, value, 0, sorted, desc, isDisplayUnvisible);
+        break;
+      case LIST_DEFAULT:
+        payload = yield call(api.fetchFiles, dir_id, 0, sorted, desc, isDisplayUnvisible);
+        break;
+      }
+      yield put(actions.initFiles(payload.data.body));
+      yield put(actions.initFileTotal(payload.data.status.total));
     }
     catch (e) {
-      console.log(e);
       const { message, errors } = errorParser(e,"一覧の取得に失敗しました");
       if (!errors.unknown) {
         yield put(commons.openException(message, errors[ Object.keys(errors)[0] ]));
