8b07d895b6af7a6d17cc294a5fd41d1c1060e54f
knb-wkm <konbu.wakayama@gmail.com>
2018-02-05 10:25:44 +0900

    非表示タグのついたファイルを非表示属性(unvisible:true)に変更するパッチ

--- server/batchs/tasks/moveInvisibleFiles.js
+++ server/batchs/tasks/moveInvisibleFiles.js
@@ -1,29 +1,17 @@
-import express from "express";
-import bodyParser from "body-parser";
 import util from "util";
 import co from "co";
 import logger from "../../lib/logger";
-import supertest from "supertest";
-import defaults from "superagent-defaults";
-
-// router
-import Router from "../../lib/routes";
 
 // models
 import Tenant from "../../lib/models/Tenant";
 import File from "../../lib/models/File";
-
-import { FILE_LIMITS_PER_PAGE } from "../../lib/configs/constants";
-
-const app = express();
-app.use(bodyParser.json());
-app.use("/", Router);
-
-const request = defaults(supertest(app));
+import Tag from "../../lib/models/Tag";
 
 const task = () => {
   co(function* () {
     try {
+      logger.info("非表示タグのついたファイルを非表示属性に変更するパッチを適用開始");
+      // console.log("非表示タグのついたファイルを非表示属性に変更するパッチを適用開始");
 
       if (! process.argv[3]) throw new Error("引数にテナント名を指定する必要があります");
       const tenant_name = process.argv[3];
@@ -31,89 +19,34 @@ const task = () => {
       const tenant = yield Tenant.findOne({ name: tenant_name });
       if (tenant === null) throw new Error(`指定されたテナントは存在しません ${tenant_name}`);
 
-      // 認証
-      const authData = {
-        account_name: "taro",
-        password: "test",
-        tenant_name: tenant_name
-      };
-
-      const authResponse = yield new Promise( (resolve, reject) => {
-        request
-          .post("/api/login")
-          .send(authData)
-          .end( (err, res) => {
-            if (err) reject(err);
-            resolve(res);
-          });
+      const unvisibleTag = yield Tag.findOne({
+        label: "非表示",
+        tenant_id: tenant._id
       });
 
-      const { user, token } = authResponse.body.body;
-
-      if (! user) throw new Error("ログイン時のユーザ情報取得に失敗");
-      if (! token) throw new Error("ログイントークンの取得に失敗");
-      request.set("x-auth-cloud-storage", token);
-
-      // 非表示タグ自身を取得
-      const tagsResponse = yield new Promise( (resolve, reject) => {
-        request
-          .get("/api/v1/tags")
-          .end( (err, res) => {
-            if (err) reject(err);
-            resolve(res);
-          });
+      const unvisibleFiles = yield File.find({
+        tags: unvisibleTag._id,
+        unvisible: false
       });
 
-      const tags = tagsResponse.body.body.filter( tag => {
-        return tag.label === "非表示";
+      // console.log(`非表示パッチ適用対象のレコード数: ${unvisibleFiles.length}`);
+      // console.log(`非表示パッチ適用対象のレコード: ${unvisibleFiles}`);
+
+      logger.info(`非表示パッチ適用対象のレコード数: ${unvisibleFiles.length}`);
+      logger.info(`非表示パッチ適用対象のレコード: ${unvisibleFiles}`);
+
+      const changedFiles = yield unvisibleFiles.map( file => {
+        file.unvisible = true;
+        return file.save();
       });
 
-      if (tags.length === 0) throw new Error("非表示タグの取得に失敗");
+      logger.info(`非表示パッチを適用したレコード数: ${changedFiles.length}`);
+      logger.info(`非表示パッチを適用したレコード: ${changedFiles}`);
+      logger.info("非表示タグのついたファイルを非表示属性に変更するパッチが適用完了");
 
-      const invisibleTagId = tags[0]._id;
-
-      // 検索項目一覧を取得
-      const searchItemsResponse = yield new Promise( (resolve, reject) => {
-        request
-          .get("/api/v1/files/search_items")
-          .end( (err, res) => {
-            if (err) reject(err);
-            resolve(res);
-          });
-      });
-
-      const searchItems = searchItemsResponse.body.body.filter( item => {
-        return item.label === "タグ";
-      });
-
-      if (searchItems.length === 0) throw new Error("displayItem名: 非表示の取得に失敗");
-
-      const searchItemId = searchItems[0]._id;
-
-      // 詳細検索にて非表示タグのついたファイルを検索する(page=0)
-      const sendBody = { page: 0, sort: null };
-      sendBody[searchItemId] = invisibleTagId;
-
-      // 1回目を取得
-      let visibleSearchResponse = yield visibleFileSearch(sendBody);
-
-      // 対象ファイルが0件の場合は終了
-      if (visibleSearchResponse.body.status.total === 0) process.exit();
-
-      let targetFiles = visibleSearchResponse.body.body;
-
-      // 非表示状態トグルのapiをコール
-      yield targetFiles.map( file => {
-        return new Promise( (resolve, reject) => {
-          request
-            .patch(`/api/v1/files/${file._id}/toggle_unvisible`)
-            .end( (err, res) => {
-              if (err) reject(err);
-              resolve(res);
-            });
-        });
-      });
-
+      // console.log(`非表示パッチを適用したレコード数: ${changedFiles.length}`);
+      // console.log(`非表示パッチを適用したレコード: ${changedFiles}`);
+      // console.log("非表示タグのついたファイルを非表示属性に変更するパッチが適用完了");
       process.exit();
     }
     catch (e) {
@@ -121,18 +54,6 @@ const task = () => {
       console.log(e);
       process.exit();
     }
-  });
-};
-
-const visibleFileSearch = (sendBody) => {
-  return new Promise( (resolve, reject) => {
-    request
-      .post("/api/v1/files/search_detail")
-      .send(sendBody)
-      .end( (err, res) => {
-        if (err) reject(err);
-        resolve(res);
-      });
   });
 };
 
