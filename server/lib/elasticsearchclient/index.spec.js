cdef6c08f5581faca54a90e1c30a9c23835ee808
K-YAMAGUCHI <quz@dummy.com>
2019-04-15 16:52:27 +0900

    コミット漏れテストコード

--- server/lib/elasticsearchclient/index.spec.js
+++ server/lib/elasticsearchclient/index.spec.js
@@ -24,7 +24,6 @@ describe("elasticsearchclientのテスト", () => {
     })
     return { result_full_text: result.hits.hits[0]._source.file.full_text, result_meta_text: result.hits.hits[0]._source.file.meta_text}
   }
-
   describe(`updateTextContents()`, async () => {
     const tenant_id = '5cb02dd57faea500c6a0acb7'
     const file_id = '5cb03ac534679e36881995d4'
@@ -41,27 +40,27 @@ describe("elasticsearchclientのテスト", () => {
       meta_text = 'yes!'
       await esClient.updateTextContents(tenant_id, file_id, meta_text, full_text)
       await test_helper.sleep(3000) //elasticsearchの更新間隔を待つ
-      const {result_full_text, result_meta_text} = await __searchTextContents(tenant_id, file_id)
-      expect(full_text).equal(result_full_text);
-      expect(meta_text).equal(result_meta_text);
+      const file = await esClient.getFile(tenant_id, file_id)
+      expect(full_text).equal(file.full_text);
+      expect(meta_text).equal(file.meta_text);
     })
     it(`null値で更新`,async () => {
       full_text = null
       meta_text = null
       await esClient.updateTextContents(tenant_id, file_id, meta_text, full_text)
       await test_helper.sleep(3000) //elasticsearchの更新間隔を待つ
-      const {result_full_text, result_meta_text} = await __searchTextContents(tenant_id, file_id)
-      expect(full_text).equal(result_full_text);
-      expect(meta_text).equal(result_meta_text);
+      const file = await esClient.getFile(tenant_id, file_id)
+      expect(full_text).equal(file.full_text);
+      expect(meta_text).equal(file.meta_text);
     })
     it(`日本語値で更新`,async () => {
       full_text = "日本語も"
       meta_text = "大丈夫！"
       await esClient.updateTextContents(tenant_id, file_id, meta_text, full_text)
       await test_helper.sleep(3000) //elasticsearchの更新間隔を待つ
-      const {result_full_text, result_meta_text} = await __searchTextContents(tenant_id, file_id)
-      expect(full_text).equal(result_full_text);
-      expect(meta_text).equal(result_meta_text);
+      const file = await esClient.getFile(tenant_id, file_id)
+      expect(full_text).equal(file.full_text);
+      expect(meta_text).equal(file.meta_text);
     })
   })
 
