f6845beaa77675da81f6886e93523607e5f9bafb
K-YAMAGUCHI <quz@dummy.com>
2019-07-31 16:51:17 +0900

    add tests for files(view, rename)

--- server/lib/controllers/files.spec.js
+++ server/lib/controllers/files.spec.js
@@ -16,7 +16,7 @@ import Tenant from "../models/Tenant";
 
 
 jest.setTimeout(40000);
-const tenant_name = 'test'
+const tenant_name = 'files_' + 'test'
 
 const sample_file = {
   "name": "file.pdf",
@@ -32,7 +32,7 @@ const sample_file = {
 describe('lib/controllers/files', () => {
   let default_res
   let mongoServer;
-  const opts = { useNewUrlParser: true }; // remove this option if you use mongoose 5 and above  
+  const opts = { useNewUrlParser: true };
   let tenant
   let user
   beforeAll(async () => {
@@ -55,13 +55,26 @@ describe('lib/controllers/files', () => {
     await mongoServer.stop();
     console.log("....terminated")
   })
+  const upload_file = async (files_array, dir_id) => {
+    const req = {
+      body: { files: files_array, dir_id }
+    }
+    const res_error_json = jest.fn()
+    const res = { user: { ...default_res.user }, json: jest.fn(), status: jest.fn(() => ({ json: res_error_json })) }
+    await controller.upload(req, res)
+    if (res.json.mock.calls.length === 0) {
+      return { success: false, errors: res_error_json.mock.calls[0][0].status.errors}
+    } else {
+      return { success: true, res: res.json.mock.calls[0][0] }
+    }
+  }
 
   describe(`upload()`, () => {
     it(`テナント情報の取得`, async () => {
       expect(tenant_name).toBe(tenant.name)
     })
 
-    it(`パラメータ不正: files is empty`, async () => {
+    it(`files is empty`, async () => {
       const req = {
         body: { files: [] }
       }
@@ -76,41 +89,123 @@ describe('lib/controllers/files', () => {
       expect(res_body.status.errors.files).toBeTruthy() //エラーが存在する
     });
     it(`1ファイル成功`, async () => {
-      const req = {
-        body: { files: [{ ...sample_file }], dir_id: tenant.home_dir_id }
-      }
-      const res_error_json = jest.fn()
-      const res = { user: { ...default_res.user }, json: jest.fn(), status: jest.fn(() => ({ json: res_error_json })) }
-      await controller.upload(req, res)
-
-      if (res.json.mock.calls.length === 0) {
-        expect(res_error_json.mock.calls[0][0].status.errors).toBe('テストを遂行できませんでした')
+      const result = await upload_file([{ ...sample_file }], tenant.home_dir_id)
+      if (result.success) {
+        expect(result.res.status.success).toBe(true)
+        expect(result.res.body.length).toBe(1) //１ファイルの結果が返る
       } else {
-        const res_body = res.json.mock.calls[0][0] //1回目の第一引数
-        expect(res_body.status.success).toBe(true)
-        expect(res_body.body.length).toBe(1) //１ファイルの結果が返る
+        expect(result.errors).toBe('* 想定外なエラー')
       }
     });
     it(`3ファイル成功`, async () => {
-      const req = {
-        body: { files: [{ ...sample_file }, { ...sample_file }, { ...sample_file }], dir_id: tenant.home_dir_id }
+      const result = await upload_file([{ ...sample_file }, { ...sample_file }, { ...sample_file }], tenant.home_dir_id)
+      if (result.success) {
+        expect(result.res.status.success).toBe(true)
+        expect(result.res.body.length).toBe(3) //3ファイルの結果が返る
+      } else {
+        expect(result.errors).toBe('* 想定外なエラー')
       }
-      const res_error_json = jest.fn()
-      const res = { user: { ...default_res.user }, json: jest.fn(), status: jest.fn(() => ({ json: res_error_json })) }
-      await controller.upload(req, res)
+    });
+  })
 
+  describe(`view()`, () => {
+    let file_id =null
+    beforeAll(async () => {
+      // 事前にファイルをアップロード
+      const result = await upload_file([{ ...sample_file }], tenant.home_dir_id)
+      if (result.success) {
+        file_id = result.res.body[0]._id
+      } else {
+        console.log(result.errors)
+      }
+    })
+    it(`file_idが空です`, async () => {
+      if (!file_id) { expect('').toBe('前処理に失敗'); return }
+      const req = {
+        params: { file_id: null}
+      }
+      const res_json = jest.fn()
+      const res = { user: { ...default_res.user }, json: jest.fn(), status: jest.fn(() => ({ json: res_json })) }
+      await controller.view(req, res)
+      expect(res.status.mock.calls[0][0]).toBe(400) // http response statusは400
+      const res_body = res_json.mock.calls[0][0] //1回目の第一引数
+      expect(res_body.status.success).toBe(false)
+      expect(res_body.status.message).toBe("ファイルの取得に失敗しました")
+    })
+    it(`成功`, async () => {
+      if (!file_id) { expect('').toBe('前処理に失敗'); return }
+      const req = {
+        params: { file_id }
+      }
+      const res_json = jest.fn()
+      const res = { user: { ...default_res.user }, json: jest.fn(), status: jest.fn(() => ({ json: res_json })) }
+      await controller.view(req, res)
       if (res.json.mock.calls.length === 0) {
-        expect(res_error_json.mock.calls[0][0].status.errors).toBe('テストを遂行できませんでした')
+        expect(res_json.mock.calls[0][0].status.errors).toBe('failed')
       } else {
         const res_body = res.json.mock.calls[0][0] //1回目の第一引数
         expect(res_body.status.success).toBe(true)
-        expect(res_body.body.length).toBe(3) //3ファイルの結果が返る
+        expect(res_body.body._id.toString()).toBe(file_id.toString()) //file_idが一致する
       }
-
-    });
+    })
   })
-  describe(`rename()`, () => {
 
+  describe(`rename()`, () => {
+    let file_id = null
+    beforeAll(async () => {
+      // 事前にファイルをアップロード
+      const result = await upload_file([{ ...sample_file }], tenant.home_dir_id)
+      if (result.success) {
+        file_id = result.res.body[0]._id
+      } else {
+        console.log(result.errors)
+      }
+    })
+    it(`file_id is empty`, async () => {
+      if (!file_id) { expect('').toBe('前処理に失敗'); return }
+      const req = {
+        params: { file_id: null }
+      }
+      const res_json = jest.fn()
+      const res = { user: { ...default_res.user }, json: jest.fn(), status: jest.fn(() => ({ json: res_json })) }
+      await controller.rename(req, res)
+      expect(res.status.mock.calls[0][0]).toBe(400) // http response statusは400
+      const res_body = res_json.mock.calls[0][0] //1回目の第一引数
+      expect(res_body.status.success).toBe(false)
+      expect(res_body.status.message).toBe("ファイル名の変更に失敗しました")
+    })
+    it(`name is empty`, async () => {
+      if (!file_id) { expect('').toBe('前処理に失敗'); return }
+      const req = {
+        params: { file_id },
+        body: {name: ''}
+      }
+      const res_json = jest.fn()
+      const res = { user: { ...default_res.user }, json: jest.fn(), status: jest.fn(() => ({ json: res_json })) }
+      await controller.rename(req, res)
+      expect(res.status.mock.calls[0][0]).toBe(400) // http response statusは400
+      const res_body = res_json.mock.calls[0][0] //1回目の第一引数
+      expect(res_body.status.success).toBe(false)
+      expect(res_body.status.message).toBe("ファイル名の変更に失敗しました")
+    })
+    it(`成功`, async () => {
+      if (!file_id) { expect('').toBe('前処理に失敗'); return }
+      const new_name = 'new_name'
+      const req = {
+        params: { file_id },
+        body: { name: new_name }
+      }
+      const res_json = jest.fn()
+      const res = { user: { ...default_res.user }, json: jest.fn(), status: jest.fn(() => ({ json: res_json })) }
+      await controller.rename(req, res)
+      if (res.json.mock.calls.length === 0) {
+        expect(res_json.mock.calls[0][0].status.errors).toBe('failed')
+      } else {
+        const res_body = res.json.mock.calls[0][0] //1回目の第一引数
+        expect(res_body.status.success).toBe(true)
+        expect(res_body.body.name).toBe(new_name) //nameが一致する
+      }
+    })
   })
 
 });
